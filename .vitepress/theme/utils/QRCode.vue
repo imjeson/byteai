f30374e109df59e1d4f4541ed435e8b03f78381c
Jeson <chenjie@imoocc.com>
2024-04-23 05:59:16 +0800

    add QRcode

---
+++ .vitepress/theme/utils/QRCode.vue
@@ -0,0 +1,24 @@
+<template>
+    <qrcode-vue :value="value" :size="size" :level="level"></qrcode-vue>
+  </template>
+  
+  <script setup>
+  import QrcodeVue from 'qrcode.vue';
+  import { ref } from 'vue';
+  
+  const props = defineProps({
+    value: {
+      type: String,
+      default: 'http://imoocc.com',
+    },
+    size: {
+      type: Number,
+      default: 50,
+    },
+    level: {
+      type: String,
+      default: 'L', // Error correction level: 'L', 'M', 'Q', 'H'
+    },
+  });
+  </script>
+  
