184890892e56a60d7eb127ff04ef8b08f52cf0a0
imjeson <jeson@imoocc.com>
2024-05-13 05:40:05 +0000

    Commit on Mon May 13 05:40:05 UTC 2024

---
+++ posts/2024-05-13-auto4-YYnW2Z.rb
@@ -0,0 +1,25 @@
+---
+
+title: '欧洲“OpenAI”叒融资6亿刀，成立1年估值60亿刀，他们只做了一件事'
+date: 2024-05-14
+author: ByteAILab
+
+---
+
+<div class="article-content__container" data-v-0aea98aa="" data-v-7f057cc4="">
+<div class="bbt-html" data-v-7f057cc4="">
+<div class="ne-render-content" data-editby="ne">
+<p>文章来源：智能涌现</p>
+<p>文｜李然 李苗</p>
+<p>编辑｜李然 </p>
+<p>“Open AI 欧洲版”Mistral AI又融到一笔巨款，让本就富裕的家庭锦上添花。</p>
+...
+<p>灵活的唯物主义六边形战士Mistral AI在欧洲“开卷”，六十亿的新估值是这条鲶鱼应得的。
+
+---
+</p>
+</div>
+</div>
+</div>
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
