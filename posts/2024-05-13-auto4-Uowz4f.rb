28b844ad3169dee1604157056025210ff3ff9bfa
imjeson <jeson@imoocc.com>
2024-05-13 23:10:05 +0000

    Commit on Mon May 13 23:10:05 UTC 2024

---
+++ posts/2024-05-13-auto4-Uowz4f.rb
@@ -0,0 +1,119 @@
+---
+
+title: '美国加州正测试ChatGPT等生成式AI，在4大部门应用'
+date: 2024-05-14
+author: ByteAILab
+
+---
+
+<div class="article-content__container">
+<div class="bbt-html">
+<div class="ne-render-content">
+<p>文章来源：<a href="https://mp.weixin.qq.com/s/7eAmXryc6iwpFLydnZ8YUA" rel="noopener nofollow" target="_blank">AIGC开放社区</a></p>
+
+<p class="ne-empty-p"><br/></p>
+
+<div class="ne-atomic">
+<div class="ne-img-wrapper" data-align="c">
+<img alt="图片来源：由GPTNB生成" src="http://www.jesonc.com/upload/3B33CB85B496C0CB6FBA4C2BD79320AD/1715580184999/FrQPeW6Dbq4WwuaiHh6ni1nyetlP.png" style="width:100%">
+<div class="ne-img-desc">图片来源：由GPTNB生成</div>
+</div>
+</div>
+
+<p class="ne-empty-p"><br/></p>
+
+<p>5月11日，美联社消息，美国加州政府正在测试ChatGPT等生成式AI，应用在税收和收费管理部、交通部、公共卫生部以及卫生与公众服务部4大部门。</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<p>测试时间6个月，<strong>为其提供技术支持的一共有5家公司，分别是OpenAI、Anthropic、谷歌、Meta和ServiceNow</strong>，测试环境将在多家云服务平台中进行，费用只需每家支付1美元。
+
+---
+</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<p>测试期间，州政府会根据实际业务的应用情况，要求对生成式AI工具进行调整，例如，特定业务领域的数据微调等，以帮助政务实现更好的工作效率和节省时间。</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<div class="ne-atomic">
+<div class="ne-img-wrapper" data-align="c">
+<img alt="" src="http://www.jesonc.com/FmrQToEuJ1Ds4QAUDuPgbXVMMpQC" style="width:554px"/>
+</div>
+</div>
+
+<p class="ne-empty-p"><br/></p>
+
+<p><strong>加州交通主管Toks Omishakin表示</strong>，平均每天有12名加州人死在公路上，我们需要利用一切可用的工具来结束公路安全隐患，到2050年实现交通事故零死亡和零重伤的目标。</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<p>用生成式AI监管交通部门的安全性是非常重要的一步。Gavin Newsom州长这一决定对改善所有加州人的生活起到了关键的推动作用。</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<div class="ne-atomic">
+<div class="ne-img-wrapper" data-align="c">
+<img alt="州长Gavin Newsom" src="http://www.jesonc.com/Fv93WYJeo2Mf_DchWIelAbaJPcaN" style="width:554px"/>
+<div class="ne-img-desc">州长Gavin Newsom</div>
+</div>
+</div>
+
+<p class="ne-empty-p"><br/></p>
+
+<p>早在2023年9月6日，加州州长Gavin Newsom便下达行政令，要求各州政务机构研究生成式AI的潜在威胁以及适合的应用场景。</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<p>Gavin认为，<strong>生成式AI的出现可以与互联网相媲美，这是一种可极大提升工作效率的变革性工具</strong>。但我们要深度了解它的各种潜在安全问题，才能进行实际业务应用。</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<p>随着ChatGPT等生成式AI工具在安全方面的防护栏越来越多，加州终于决定在4个政务繁重的部门开始测试。</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<p><strong>以交通运输部门为例：</strong>交通运输部门可以使用集成了生成式AI（例如，GPT-4V）的监控系统，可实时分析交通数据，通过摄像头监控交通流量和行为模式，识别潜在的危险行为或交通拥堵情况。当AI发现潜在的风险时，可以立即通知附近的司机减轻交通压力。</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<p>还可以通过生成式AI技术，对公交车、火车等公共交通工具的运行状态进行实时跟踪和调度。系统可以通过预测乘客流量和车辆位置来优化运行计划和服务安排。</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<div class="ne-atomic">
+<div class="ne-img-wrapper" data-align="c">
+<img alt="" src="http://www.jesonc.com/FpWMdesUtFf03RKsMWPmtKvxeUEZ" style="width:416px"/>
+</div>
+</div>
+
+<p class="ne-empty-p"><br/></p>
+
+<p>税收和收费管理领域，可通过生成式AI开发一个虚拟助手，可自动分析纳税申报，并通过比对历史数据、检测异常模式来识别可疑活动。一旦AI检测到可能的欺诈行为，可自动标记案件供人工复核。</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<p>也可以将生成式AI集成在客服中，开发一个AI虚拟税务顾问，为民众提供7*24的自助咨询服务，可解答有关税率、抵扣、退税和合规性等问题，并根据最新的税法变动进行更新。</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<p><strong>加州税务和收费机构主管Nick Maduros表示</strong>，2023年我们机构管理着 40 多个项目，并接到了超过66万个企业电话。希望通过部署生成式AI来监听这些电话，并实时提取州税法的关键信息，让工作人员能够更快地回答问题，而不必自己查找各种信息。</p>
+
+<p class="ne-empty-p"><br/></p>
+
+<p class="ne-empty-p"><br/></p>
+
+<p data-textalign="align-c"><code>本文素材来源加州官网、美联社、ABC，如有侵权请联系删除</code></p>
+<p data-textalign="align-c" rel="nofollow noopener"><code>END</code></p>
+</div>
+</div> 
+
+<div class="bbt-html">
+
+</div>
+</div>​
+
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
