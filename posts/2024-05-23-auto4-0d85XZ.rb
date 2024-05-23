4d54ba47e1d2f3d18cc505140682dea8036860e3
imjeson <jeson@imoocc.com>
2024-05-23 23:10:05 +0000

    Commit on Thu May 23 23:10:05 UTC 2024

---
+++ posts/2024-05-23-auto4-0d85XZ.rb
@@ -0,0 +1,40 @@
+---
+
+title: '大模型卷价格，向量数据库“卷”什么？'
+date: 2024-05-24
+author: ByteAILab
+
+---
+
+“不是我不明白，这世界变化快”，崔健在20世纪写下的这句歌词，放在公有云市场也同样适用。技术风向的变化之快，让不少人感到惊讶。
+
+---
+
+
+2023年初大模型、生成式 AI的起飞，也带来了向量数据库的爆火。投融资项目爆发式增长，头部公有云厂商都推出了AI向量数据库的相关产品。
+
+然而一年狂飙之后，市场逐渐退潮，全球最著名的 AI 项目之一AutoGPT在2024年初宣布，不再使用向量数据库。因此有人质疑，向量数据库又凉了？
+
+...
+
+---
+
+<div class="ne-atomic"><div class="ne-img-wrapper" data-align="c"><img alt="" src="http://www.jesonc.com/FiphwzQ4rpKlHrgWhfirCXHIEjvq" style="width:100%"/></div></div>
+
+...
+
+---
+
+<div class="ne-atomic"><div class="ne-img-wrapper" data-align="c"><img alt="" src="http://www.jesonc.com/FsnY5j4DfoBIZzG0cu3u7gHXAu_Z" style="width:100%"/></div></div>
+
+...
+
+---
+
+<div class="ne-atomic"><div class="ne-img-wrapper" data-align="c"><img alt="" src="http://www.jesonc.com/Fl_0gZWEnblF1iYY_IEwalm2CcHy" style="width:100%"/></div></div>
+
+...
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
