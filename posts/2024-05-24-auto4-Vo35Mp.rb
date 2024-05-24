06169ff986b567b20b7e4ce3e8bcfecc11020224
imjeson <jeson@imoocc.com>
2024-05-24 23:10:06 +0000

    Commit on Fri May 24 23:10:06 UTC 2024

---
+++ posts/2024-05-24-auto4-Vo35Mp.rb
@@ -0,0 +1,47 @@
+---
+title: 'AI生图、扩图、去水印……全免费！「Sora 平替」Viva 玩了把大的'
+date: 2024-05-25
+author: ByteAILab
+
+---
+
+<div class="ne-render-content">
+<div class="ne-atomic">
+<div class="ne-img-wrapper" data-align="c">
+<img alt="图片来源：由GPTNB生成" src="http://www.jesonc.com/upload/3B33CB85B496C0CB6FBA4C2BD79320AD/1716530971014/Fk7-Cw3MkolLa2LlmHQdwjGh-5u6.jpg" style="width:100%">
+<div class="ne-img-desc">图片来源：由GPTNB生成</div>
+</div>
+</div>
+
+提示词：perfectly styled alfresco painted English cottage outdoor pink country style kitchen with rustic elements. House and garden magazine photoshoot
+
+通通免费！AI功能「大杂烩」Viva，抢了美图秀秀的饭碗。
+国内的美图秀秀有 AI 视觉设计「大杂烩」之称，只要是市面上比较火的图片处理功能，都能在美图秀秀上找到身影。
+
+---
+
+而 Viva 堪称国外版「美图秀秀」，除了视频生成功能外，还有文生图、图生文、智能抠图、AI 扩图等，而且是把免费进行到底！
+
+文生图：Midjourney 被「偷家」
+Viva 野心不小，既想和 Sora 一较高下，又要抢 Midjourney 和 Stable Diffusion 的地盘。
+众所周知，Midjourney 需要付费使用，每月至少 20 美元，而且还需要在 Discord 中一遍遍召唤机器人。
+Stable Diffusion 虽然免费，但需要部署，对于小白用户很不友好。
+而 Viva 打出来的招牌就是「免费且简单」。
+Viva 官网链接：https://vivago.ai
+
+打开 Viva 官网，选择「Text to image」，然后输入提示词即可。
+为了呈现效果更好，我们还可以设置长宽比、Negative prompt （否定提示词）以及类型标签。
+Viva 提供颜色、艺术家、风格、发型、环境、照明等 13 种类型标签。
+
+我们从 Midjourney 官网上扒下来几个 prompt，然后丢给 Viva，看它能整出什么花。（左为 Viva，右为 Midjourney）
+
+Prompt 1：Soul band with brass instruments, warm and intimate atmosphere,2:3。
+中文提示词 1: 灵魂乐队用铜管乐器演奏，气氛温馨，2:3。
+![](http://www.jesonc.com/Flsr9Cb2f1lJOGxznxteMb1DJChn)
+...
+
+本文链接：<a href="https://www.aixinzhijie.com/article/6845878">https://www.aixinzhijie.com/article/6845878</a>
+转载请注明文章出处
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
