21937a8ebfc351b8086c0dd1286227d3a1630205
imjeson <jeson@imoocc.com>
2024-05-25 23:10:05 +0000

    Commit on Sat May 25 23:10:04 UTC 2024

---
+++ posts/2024-05-25-auto4-39VwKq.rb
@@ -0,0 +1,29 @@
+
+
+---
+---
+title: '今晨！马斯克最新言论：AI是我最恐惧的事物，它将取代人类所有工作'
+date: 2024-05-26
+author: ByteAILab
+
+---
+
+<div class="article-content__container" data-v-0aea98aa="" data-v-7f057cc4="">
+  <div class="bbt-html" data-v-7f057cc4="">
+    <div class="ne-render-content" data-editby="ne">
+      <p>文章来源：<a href="https://mp.weixin.qq.com/s/e-PbROhgeg91zkSffydv2Q" rel="noopener nofollow" target="_blank">钛媒体</a></p>
+      <p>作者｜林志佳</p>
+      <p>编辑｜胡润峰</p>
+      
+      <!-- Body content here without the html tags from the source -->
+      <p>钛媒体App 5月24日消息，今天凌晨，特斯拉CEO马斯克（Elon Musk）通过视频连线参加在法国巴黎举办的“欧洲科技创新展览会”（VivaTech 2024），主要谈 AI、电动车、创业相关话题。</p>
+      
+      ...
+
+    </div>
+  </div>
+</div>
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
