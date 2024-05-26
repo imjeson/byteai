b89873e672ddd82a9573509473f0dcf46cc14e68
imjeson <jeson@imoocc.com>
2024-05-26 19:10:06 +0000

    Commit on Sun May 26 19:10:06 UTC 2024

---
+++ posts/2024-05-26-auto3-TPPPMF.rb
@@ -0,0 +1,28 @@
+```
+---
+
+title: 'LABScon 2024宣布终极网络安全研讨会'
+date: 2024-05-27
+author: ByteAILab
+
+---
+
+Leading Cybersecurity Researchers to Gather in Scottsdale, AZ September 18-21
+SentinelOne (NYSE: S), a global leader in AI security, today announced the return of LABScon, the premier conference dedicated to advancing cybersecurity research for the benefit of global digital defense. The exclusive, invite-only event will take place at Mountain Shadows Resort in Scottsdale, AZ September 18-21, and researchers are encouraged to submit original findings via the conference website to secure their spot today.
+“The growing complexity of cyberattacks and diversity of threat actors underscore a clear truth: no single entity can effectively shoulder the burden of defense alone,” said Juan Andres Guerrero-Saade, AVP of Research at SentinelOne. “LABScon has emerged as an indispensable forum, drawing together the security research braintrust of academia, governments, and private industry to exchange ideas, collaborate, and decisively shift the balance towards collective defense.”
+Hosted by SentinelLabs, a world-class team of security researchers that uncovers critical vulnerabilities, new attack vectors, malware strains, and threat actors, LABScon 2024 will feature:
+Keynotes and presentations from esteemed threat researchers and leading cybersecurity experts
+Interactive breakout sessions
+Ample networking opportunities
+Leading global security vendors have already signed on to sponsor the event, including: Luta Security, Binarly, Cisco Talos, Dreadnode, GreyNoise, and the Alperovitch Institute.
+Additional sponsorship opportunities are available. For more information, contact info@labscon.io.
+Researchers who would like to be considered for participation in LABScon 2024 can submit papers here through June 21. Submissions will be reviewed by the LABScon Program Committee, which includes top threat researchers and cybersecurity experts from Google, DARPA, Netflix, Johns Hopkins University, and SentinelLabs。![图片](https://ai-techpark.com/wp-content/uploads/2024/05/LABScon-960x540.jpg){ width=60% }
+
+---
+
+注意：Title、Date、Body 三个部分的内容，放入到对应的位置。最后只需要输出为Makedown源文件格式内容。
+
+---
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
