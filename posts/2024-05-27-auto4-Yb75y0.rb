cb45d71e63c0aef1c8553344a8617e23a3345489
imjeson <jeson@imoocc.com>
2024-05-27 23:10:05 +0000

    Commit on Mon May 27 23:10:05 UTC 2024

---
+++ posts/2024-05-27-auto4-Yb75y0.rb
@@ -0,0 +1,40 @@
+```
+---
+
+title: '最新！今年一季度，全球AI领域融资总额达1564亿，同比下降31.2%'
+date: 2024-05-28
+author: ByteAILab
+
+---
+
+文章来源：[钛媒体AGI](https://www.aixinzhijie.com/article/6845892)  
+作者｜林志佳  
+
+![图片来源：由GPTNB生成](http://www.jesonc.com/upload/3B33CB85B496C0CB6FBA4C2BD79320AD/1716772678771/FjXVJNqRNCbX6tidir8xUlqlZYfJ.png)  
+
+**钛媒体App 5月26日消息，近日，市场研究机构Pitchbook发布2024年Q1全球人工智能（AI）与机器学习领域的最新投融资数据报告。**  
+
+报告显示，2024年第一季度（1-3月），全球AI领域共计完成1779笔融资交易，筹集的风险投资总额高达216亿美元（约合人民币1564.27亿元），交易价值环比下降7.8%，同比下降31.2%。
+
+---
+  
+
+![图片来源：由GPTNB生成](http://www.jesonc.com/FtSmcMXyEEX45-SuKly-7fJQL67y)  
+
+其中，本季度融资交易当中，有53亿美元（约合人民币383.83亿元）来自于 Anthropic、Mistral Al 和 xAl 的基础模型公司的大型交易。而截至2024年第二季度，xAl和Mistral Al 将延续大型融资交易趋势。  
+
+同时报告称，投资者对早期领导者的关注体现在早期投前估值中位数超过后期中位数，在78个交易企业的记录估值中达到5500万美元。  
+
+...（部分内容省略）
+
+![图片来源](http://www.jesonc.com/FqlezAj9Zoxbryrr_tYTF83DueLs)  
+
+商业化层面，Databricks在2023年的收入同比增长50%，接近20亿美元，合作伙伴包括微软Microsoft Azure 和亚马逊AWS等。而从长远来看，Databricks这种基于数据湖架构对流数据的增长机会，将比关系数据库现有企业获得更高的估值。  
+
+...（部分内容省略）
+
+---
+
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
