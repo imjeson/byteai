7960c440d14a99b30ca718ff4187958f31d490f5
imjeson <jeson@imoocc.com>
2024-05-31 23:10:06 +0000

    Commit on Fri May 31 23:10:06 UTC 2024

---
+++ posts/2024-05-31-auto4-p9QuV4.rb
@@ -0,0 +1,51 @@
+---
+title: '速递｜BVP 牵头 Perplexity 2.5 亿美元融资！最新估值 30 亿美元&CoreWeave 即将在 2025 年 IPO！'
+date: 2024-06-01
+author: ByteAILab
+
+---
+
+<div class="article-content__container">
+<div class="bbt-html">
+<div class="ne-render-content">
+<p>文章来源：<a href="https://mp.weixin.qq.com/s/jle1w4HWH3PaHzHS10JbOQ" rel="noopener nofollow" target="_blank">Newin有新</a></p>
+<p class="ne-empty-p"><br/></p>
+<div class="ne-atomic">
+<div class="ne-img-wrapper" data-align="c"><img alt="图片来源：由无界AI生成" src="https://appserversrc.8btc.cn/upload/3B33CB85B496C0CB6FBA4C2BD79320AD/1717034690862/FuaShxvc2lVLo9CyRsVAvGP87tUc.png" style="width:100%"><div class="ne-img-desc">图片来源：由无界AI生成</div></div></div>
+<p class="ne-empty-p"><br/></p>
+<blockquote>BVP 牵头 Perplexity 2.5 亿美元融资！最新估值 30 亿美元</blockquote>
+<p>根据 The Information 最新报道，BVP 正在牵头对 Perplexity 2.5 亿美元的投资，最新估值 30 亿美元。<em><strong>有新</strong></em> 在 4 月份就曾分享过<a href="http://mp.weixin.qq.com/s?__biz=Mzg3NDkyMTQ5Mw==&mid=2247492224&idx=1&sn=b4061409a6bf2e2f96a8031783a31f2f&chksm=cecbc9faf9bc40ecfc5e717f1f8923accbb9d25c39db0a1d5b10d190fb5db860c1d68c4a6b44&scene=21#wechat_redirect" rel="noopener nofollow" target="_blank"> </a>Perplexity 最新估值达 30 亿美元！一个月翻两倍。
+
+---
+</p>
+<p class="ne-empty-p"><br/></p>
+<h4><strong>▍相比年初估值翻两倍</strong></h4>
+<p>据悉，这笔新投资是 Perplexity 自 2023 年 3 月以来的第四轮融资，是今年早些时候估值的三倍，其他投资者包括 Databricks、NEA、AIX Ventures、Elad Gil 以及 Nat Friedman。</p>
+<p>今年 1 月，Perplexity 刚刚筹集近 7400 万美元，估值达到 5.4 亿美元，3 月初，Perplexity 便以 10 亿美元的估值完成了 5600 万美元的融资。</p>
+<p>Perplexity 自 2022 年成立以来，已经吸引了一长串投资者，包括 IVP、NEA、红杉、Bessemer、Kindred 等著名风险投资公司；Nvidia、Databricks、Bezos Expeditions 等战略支持者；以及 Jeff Bezos、Meta 首席 AI 科学家 Yann LeCun、Naval Ravikant、Susan Wojcicki、Elad Gil、Nat Friedman、Hugging Face 的 Clément Delangue 等知名人士。</p>
+<p class="ne-empty-p"><br/></p>
+<h4><strong>▍未来或是 Netflix 混合订阅模式</strong></h4>
+<p>Perplexity 成立不到两年，团队不足 50 人，但用户群已增长至数千万。</p>
+<p>CTO Denis Yarats 曾表示，Perplexity 当前的订阅是主要模式，未来肯定会有其他的东西，不排除广告，但像 Google 现在所做的广告形式可能不是 Perplexity 要做的，可以通过有效方法让广告成为对用户有益的方式，如果广告有帮助的话，人们其实并不介意广告。</p>
+<p>熟悉 Netflix 的朋友应该知道，自去年以来 Netflix 推出了广告模式，相较于纯净无广告的付费订阅，低价版+广告的形式进一步促进了付费用户的规模，业绩也是持续攀升。</p>
+<p>据彭博社报道，Perplexity 今年迄今为止已处理了 7500 万次查询，目前 ARR 为 2000 万美元。快速连续的融资活动反映了公司利用客户和投资者兴趣的战略。</p>
+<p>Perplexity 去年曾考虑出售自己，理由是担心训练 AI 模型的成本以及与谷歌等巨头的竞争。</p>
+<p>然而，最新一轮融资动态表明，投资者仍然继续押注 Perplexity 的前景，特别是谷歌等老牌搜索巨头市场份额每况愈下的形势下。</p>
+<p class="ne-empty-p"><br/></p>
+<blockquote>CoreWeave 即将在 2025 年 IPO！</blockquote>
+<p class="ne-empty-p" rel="nofollow noopener"><br/></p>
+<p>根据 The Information 最新报道，CoreWeave 准备于 2025 年 IPO！5月 初，Coreweave 才宣布完成 11 亿美元 C 轮融资！当时最新估值 190 亿美元；随后 5 月 19 日，CoreWeave 宣布从贝莱德完成 75 亿美元债务融资！CEO 表示还要继续筹钱，离上一轮 11 美元融资仅过去两周。</p>
+<p>CoreWeave 成立于七年前，最初是一家加密货币挖矿公司，目前运营着至少 14 个数据中心，这些数据中心都配备了 Nvidia 备受追捧的 AI 芯片，将计算能力出租给初创公司和其他开发 AI 模型和软件的公司。</p>
+<p>通过围绕 Nvidia 芯片设计数据中心，CoreWeave 表示其服务器可以比竞争对手的云服务更快、更便宜地处理 AI 模型。</p>
+<p>过去一年，CoreWeave 通过股权和债务融资了约 100 亿美元，以支付购买 Nvidia 芯片和建设数据中心的费用。其中包括来自 Coatue 和 Magnetar 等华尔街投资者的 10 多亿美元股权融资，以及本月从黑石、凯雷和贝莱德筹集的 75 亿美元债务融资。Nvidia 也是其支持者之一。</p>
+<p>Coreweave 高管曾表示，2024 年营收增长 10 倍，约 24 亿美元。此外，CoreWeave 于 11 月举行了自己的二次股票发行，允许员工出售价值 6.42 亿美元的股票，交易由摩根士丹利 (Morgan Stanley) 负责。迄今为止，CoreWeave 已与 Fenwick &amp; West 律师事务所合作开展私募融资工作。</p>
+</div>
+</div> <!-- --> <div class="bbt-html">
+<p>
+  本文链接：<a href="https://www.aixinzhijie.com/article/6845924">https://www.aixinzhijie.com/article/6845924</a>
+</p>
+</div></div>。请先只将Body部分的内容去除“      本文链接：<a href="https://www.aixinzhijie.com/article/6845498" data-v-7f057cc4="">https://www.aixinzhijie.com/article/6845498</a><br data-v-7f057cc4="">
+      转载请注明文章出处”内容，并转化为不带html标签（注：图片链接展示需要保留）的Makedown的内容。再整体按照如下Makedown格式标准输出。
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
