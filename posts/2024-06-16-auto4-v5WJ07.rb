3ef1be583091f87a9fc19ce480a473ccdaa7aee7
imjeson <jeson@imoocc.com>
2024-06-16 23:10:05 +0000

    Commit on Sun Jun 16 23:10:05 UTC 2024

---
+++ posts/2024-06-16-auto4-v5WJ07.rb
@@ -0,0 +1,69 @@
+---
+title: '560亿美元巨额薪酬方案即将落锤，但马斯克已经迫不及待地进行了“剧透”'
+date: 2024-06-17
+author: ByteAILab
+
+---
+
+<div class="article-content__container" data-v-0aea98aa="" data-v-7f057cc4="">
+<div class="bbt-html" data-v-7f057cc4="">
+<div class="ne-render-content" data-editby="ne">
+<p>文章来源：腾讯科技</p>
+<p></p>
+<div class="ne-atomic">
+<div class="ne-img-wrapper" data-align="c">
+<img alt="图片来源：由GPTNB生成" src="http://www.jesonc.com/upload/3B33CB85B496C0CB6FBA4C2BD79320AD/1718325839673/FuGTRvzZH8HHLNULOXlz3vjrlR6Y.png" style="width:100%">
+<div class="ne-img-desc">图片来源：由GPTNB生成</div>
+</div>
+</div>
+<p></p>
+<p>腾讯科技讯 6月13日消息，据国外媒体报道，特斯拉的股东正站在一个关键的十字路口上，他们将投票决定是否支持埃隆·马斯克（Elon Musk）的领导才能以及他前所未有、高达11位数的薪酬计划。对于这家汽车制造商而言，这是一个至关重要的时刻。
+
+---
+</p>
+<p>马斯克是一个乐于冒险著称的领导者，他承诺要将100万人送上火星（SpaceX），用人形机器人填满工厂（Tesla Bot），并在地下构建一个高速公路网络（The Boring Company）。然而，迄今为止，这些大胆的目标尚未转化为实际的回报。</p>
+<p>但六年前，马斯克做出了一个与他个人利益息息相关的决定，即将自己的特斯拉薪水与公司未来十年的财务目标紧密挂钩，包括将公司的市值从590亿美元提升到6500亿美元。当时，这些目标被评论员们普遍视为“令人震惊”且“极难实现”。如果未能达成，他将不会从公司获得任何薪水。</p>
+<p>特斯拉董事会在2018年通过了这一计划。然而，一位名叫理查德·托内塔（Richard Tornetta）的重金属鼓手，尽管仅持有九股特斯拉股票，却对此持反对态度。同年6月，他选择提起诉讼，声称这一薪酬方案对像他这样的投资者不公平。到2022年，此案在特拉华州法院审理，那时马斯克距离达成获得巨额奖金的里程碑仅一步之遥。然而，今年1月，法官站在了托内塔一边，废除了这个“不可思议”的薪酬计划，并将参与制定此计划的董事描述为“马斯克仆人”。</p>
+<p>马斯克在2023年底成功实现了12个令人震惊的目标，随后特斯拉市值一度突破万亿美元大关。如今，尽管特拉华州法官做出了裁决，但马斯克仍然坚持要求获得报酬。在美国当地时间周四的特斯拉年度股东大会上，股东们被要求再次进行投票，以决定是否应该给予马斯克价值达560亿美元的薪酬包。对于股东们而言，他们面临着一个价值560亿美元的问题：马斯克是否值得拥有这份巨额报酬？</p>
+...
+<p>总体而言，特斯拉的投资者对马斯克的薪酬计划持有不同的观点。支持者认为，马斯克对公司的巨大贡献应获得与其相符的丰厚回报。而反对者则表达了对于薪酬计划庞大规模的担忧，以及这一计划可能给股东带来的权益稀释影响。此次投票不仅将深刻影响特斯拉的未来发展方向，更是对马斯克领导地位的一次重要考验。</p>
+...
+<p>特斯拉正在反诉美国国家劳工关系委员会（NLRB）。此前，NLRB裁定马斯克在2018年的一条推特上违反了劳动法，该推文声称特斯拉员工若组织工会，可能会失去股票期权。今年早些时候，第五巡回上诉法院就这一案件进行了口头辩论，但由17名法官组成的小组尚未作出最终裁决。</p>
+
+<div class="ne-atomic">
+<div class="ne-img-wrapper" data-align="c">
+<img alt="" src="http://www.jesonc.com/FhyUBOHpXHYt44OWkzBJNgyYlLX8" style="width:677px"/>
+</div>
+</div>
+<p data-textalign="align-c"><strong><u>虽然没到最终结果，</u></strong></p>
+<p data-textalign="align-c"><strong><u>根据股东实时投票统计，两项都确认将会通过</u></strong></p>
+<p>截至北京时间6月13日上午11时，两项股东投票即时数据已经显示达到稳赢线。</p>
+
+<div class="ne-atomic">
+<div class="ne-img-wrapper" data-align="c">
+<img alt="" src="http://www.jesonc.com/Fk6FBPeX7CqHHjrQlWtr3_sxvgrD" style="width:677px"/>
+</div>
+</div>
+<p>薪酬方案投票</p>
+
+<div class="ne-atomic">
+<div class="ne-img-wrapper" data-align="c">
+<img alt="" src="http://www.jesonc.com/FrwJEM9IaMQ1Rn4YSrxtXYR0mKGE" style="width:677px"/>
+</div>
+</div>
+<p>建厂投票</p>
+
+<p>马斯克已在推特上发帖庆祝。（编译/金鹿）</p>
+
+<div class="ne-atomic">
+<div class="ne-img-wrapper" data-align="c">
+<img alt="" src="http://www.jesonc.com/Fni1_kQ5qx8XJ6Q9Au8tcGarms1t" style="width:677px"/>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
