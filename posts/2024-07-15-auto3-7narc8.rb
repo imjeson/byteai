1702b8360b00e4b1b41d27050430dc672cf3d204
imjeson <jeson@imoocc.com>
2024-07-15 19:10:06 +0000

    Commit on Mon Jul 15 19:10:05 UTC 2024

---
+++ posts/2024-07-15-auto3-7narc8.rb
@@ -0,0 +1,30 @@
+```
+---
+
+title: 'Axonius继续扩张高管团队，任命两位新职'
+date: 2024-07-16
+author: ByteAILab
+
+---
+
+Nick Degnan被任命为首席营收官，Rob Casselman被任命为首席客户官，两人是Axonius历史上的第一位
+领先的网络安全资产管理和SaaS管理解决方案提供商Axonius今天宣布任命Nick Degnan为Axonius历史上第一位首席营收官（CRO），Rob Casselman为Axonius历史上第一位首席客户官（CCO）。![图片](https://ai-techpark.com/wp-content/uploads/2024/07/Axonius-Continues-960x540.jpg){ width=60% }
+
+---
+
+
+此次任命发生在Axonius发展关键时期。今年5月，该公司宣布了新的首席营销官和首席财务官，紧随今年3月进行的2亿美元E轮融资和2023年底1亿美元ARR的里程碑之后。
+
+“对于Axonius来说，这是一个历史性的一年。我很高兴通过任命Rob为我们的首位首席客户官，以及任命Nick为我们的首位首席营收官来继续这股势头。随着Rob将带来的更深入的客户关注和Nick所展示的，我们将增强客户旅程，并继续为我们的客户提供价值。” Axonius的首席执行官兼联合创始人Dean Sysman说。
+
+作为Axonius以前的销售高级副总裁（SVP），Degnan利用自己10多年的销售领导经验建立并迅速扩大全球销售和渠道团队。在过去的18个月中，作为销售高级副总裁，Degnan在加深客户关系和加强区域对齐方面发挥了重要作用，所有这些都对Axonius去年实现1亿美元ARR的道路至关重要。
+
+“在Axonius的时光令人难忘，我很感谢有机会以新的身份为我们的团队和客户提供服务。” Axonius的CRO Nick Degnan说。“我们有可能拥有最好的团队共同为我们的成长和我们客户的成功而努力，我知道我们已经做好了在这个市场上持续获胜的准备。”
+
+Casselman来自Zscaler，最近担任解决方案咨询高级副总裁，在那里领导了一个600人的团队。他此前在思科Meraki担任过多个职务，在那里他最近担任销售副总裁。他将与Nick Degnan密切合作，深化客户细分和优化Axonius销售团队。
+
+“我与许多网络安全和科技组织合作过，Axonius在其能力和方法上确实独一无二。” Axonius的CCO Rob Casselman说。“网络安全是一个快速发展的领域，您需要时刻关注客户的需求，通常需要在客户之前就了解。Axonius团队正处于利用其不断增长的技术平台并使其客户实现惊人成就的绝佳位置。我很高兴成为其中一员，并为Axonius以及其客户和合作伙伴的持续成功做出贡献。”
+
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
