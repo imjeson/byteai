2c680fa4517ceacf2839544aa8f7f3dca451bc09
imjeson <jeson@imoocc.com>
2024-07-18 23:10:05 +0000

    Commit on Thu Jul 18 23:10:05 UTC 2024

---
+++ posts/2024-07-18-auto4-uQT4jf.rb
@@ -0,0 +1,20 @@
+---
+title: 'AI生成视频，究竟有多癫狂?'
+date: 2024-07-19
+author: ByteAILab
+
+---
+
+<div class="article-content__container" data-v-0aea98aa="" data-v-7f057cc4="">
+<div class="bbt-html" data-v-7f057cc4="">
+<div class="ne-render-content" data-editby="ne">
+来源：[亿欧网](https://mp.weixin.qq.com/s/DgL_KZKGxlQhIPTUEGaXcQ)
+
+最近，各路视频生成AI放出的Demo让人眼花缭乱。“容嬷嬷喂紫薇吃香肠”“唐僧啃鸡腿、吃汉堡”“悟空掏手枪”“尔康失态”等“名场面”在各大短视频平台广为流传。
+
+---
+
+...
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
