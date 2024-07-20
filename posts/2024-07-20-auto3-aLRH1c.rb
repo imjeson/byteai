0cb14fc9e83e41f76d9c5cbfb4efb2e663ebe532
imjeson <jeson@imoocc.com>
2024-07-20 19:10:06 +0000

    Commit on Sat Jul 20 19:10:06 UTC 2024

---
+++ posts/2024-07-20-auto3-aLRH1c.rb
@@ -0,0 +1,25 @@
+```
+---
+
+title: 'EdgeRunner AI以550万美元种子轮启动边缘生成式AI技术'
+date: 2024-07-21
+author: ByteAILab
+
+---
+
+EdgeRunner AI，一家新兴的为边缘构建生成式AI的初创公司，今天宣布正式启动，并完成了一笔550万美元的种子轮融资，由Four Rivers Group领投，Madrona Ventures和战略天使参与。![图片](https://ai-techpark.com/wp-content/uploads/2024/07/EdgeRunner-960x540.jpg){ width=60% }
+
+---
+这笔资金将用于构建EdgeRunner AI的创新平台，该平台采用最先进的透明模型，设计成能够在任何设备或硬件上无缝运行，而无需互联网访问。这项技术将改变企业和组织负责任地部署生成式AI的方式，为它们提供超个性化和特定领域的用例。
+
+“当前生成式AI的挑战在于广泛适用性，而未解决特定的业务需求，”EdgeRunner AI的联合创始人兼首席执行官Tyler Xuan Saltsman表示。“在EdgeRunner，我们赋予组织拥有他们的AI的力量。我在东欧的军事经历中看到了在孤立环境中需要个性化、特定领域和安全的AI的需求。黑盒专有模型更加倾向于云而不是用户。了解这些不透明模型内部的偏见对于可用性和安全性都至关重要而复杂。我们的方法确保了模型的完全透明性，从权重和代码到结构和数据集，无需移动数据，可以在任何设备和芯片上运行。”
+
+通过开发针对任何芯片进行优化的小型、任务特定的开放模型，EdgeRunner AI将SLM（Small Language Models）提升为UELMs（Ultra-Efficient Language Models）。这使得组织可以在任何设备或硬件上本地运行AI模型，从而实现提高性能、增强数据隐私、接近零延迟和减少能耗，为更好、更可持续的未来做出贡献。
+
+“企业和政府并不需要GPT-5或AGI。他们需要的是负责任地部署生成式AI，以利用多个小型、任务特定的开放模型共同解决真实的业务挑战，从而创造群体智能，”EdgeRunner AI的联合创始人兼首席运营官Colton Malkerson表示。
+
+探索AITechPark，获取人工智能、物联网、网络安全、AITech新闻的最新进展以及来自行业专家的深入见解！
+
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
