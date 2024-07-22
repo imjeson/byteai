c96e47f311140ae5f02221b45843fcc00d805536
imjeson <jeson@imoocc.com>
2024-07-22 19:10:06 +0000

    Commit on Mon Jul 22 19:10:06 UTC 2024

---
+++ posts/2024-07-22-auto3-iofYFV.md
@@ -0,0 +1,23 @@
+---
+title: 'SecurityScorecard获得TX-RAMP临时认证'
+date: 2024-07-23
+author: ByteAILab
+
+---
+
+得克萨斯州机构采用信用等级评分系统以提升网络安全弹性
+SecurityScorecard今日宣布已获得得克萨斯风险和授权管理计划（TX-RAMP）临时认证。![图片](https://ai-techpark.com/wp-content/uploads/2024/07/SecuritySco-960x540.jpg){ width=60% }
+
+---
+作为TX-RAMP供应商，得克萨斯州机构可利用SecurityScorecard的标准化方法来衡量提供经验证、认证和成熟供应链网络防御的网络安全性。
+SecurityScorecard的安全评分在全球公共部门机构中是公认和信任的客观、数据驱动的网络安全性能指标。公共行业客户包括美国国土安全部、运输安全管理局、加拿大网络安全中心、全国县级协会和美国州立银行监管主管会议。
+SecurityScorecard赋予数百个公共部门机构权力，以识别和解决供应链漏洞，确保更具弹性的数字生态系统。SecurityScorecard于2023年10月获得了FedRAMP®预备指定，并于2024年2月获得了StateRAMP®授权。
+SecurityScorecard的全球政府事务和公共政策副总裁李杰表示：“令人震惊的是，75%的第三方入侵目标是软件和技术供应链，突显出对整个政府景观进行持续风险评估的迫切需求。随着对州机构和关键基础设施的持续攻击，实时量化网络风险的标准化度量是数据驱动决策的关键。这种方法增强了清晰、可衡量的法规基础，最终提升网络安全弹性。”
+由得克萨斯信息资源部创建，TX-RAMP提供了给供应商如SecurityScorecard的安全评估、认证和持续监控的标准化方法。
+对于存储、处理或传输州机构机密数据且服务被判定为中等或高影响信息资源的云服务提供商，需要符合TX-RAMP Level 2标准。基线的评估标准基于NIST 800-53中等影响基线控制，并结合了FedRAMP和StateRAMP的额外参数。
+附加资源
+访问我们的网站，了解更多SecurityScorecard的政府解决方案以及TX-RAMP、StateRAMP和FedRAMP的信息。
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
---
+++ posts/2024-07-22-auto3-vkdWMU.rb
@@ -0,0 +1,27 @@
+```
+---
+
+title: 'Celonis任命Greg Czajkowski担任执行副总裁工程'
+date: 2024-07-23
+author: ByteAILab
+
+---
+
+Celonis宣布任命Grzegorz（Greg）Czajkowski为执行副总裁工程。![图片](https://ai-techpark.com/wp-content/uploads/2024/07/Celonis-A-960x540.jpg){ width=60% }
+
+---
+Czajkowski将监督公司全球工程团队，继续推进其领先市场的流程智能平台。在加入Celonis之前，Czajkowski在Snowflake担任了五年的工程与支持执行副总裁，负责监督全球工程团队，负责开发和交付Snowflake Data Cloud。
+
+在他的新角色中，Czajkowski将利用其深厚的行业领先平台和基础设施解决方案建设经验，进一步增强Celonis的流程智能平台。他将领导工程和产品团队，引导他们在继续创新和增强平台基础的同时，确保无与伦比的规模和速度，并进一步利用人工智能获得更深入的流程见解和背景信息。
+
+Czajkowski表示：“我非常高兴能加入团队。Celonis创建了一套高度复杂的流程智能平台，使其客户能够实现可观的价值并推动可持续的企业转型。我对帮助各行各业和地区的组织实现规模化流程优化着实着迷。这家公司的潜力确实非凡——每个人都能从Celonis的技术中受益。我期待进一步推动Celonis已经开发的全面流程优化和智能自动化，并且很高兴能与团队合作，充分实现其潜力。”
+
+Celonis的联合创始人兼联合首席执行官Alex Rinke表示：“Greg在工程领导和创新方面的卓越经验使他成为我们的完美选择。我们非常高兴欢迎Greg加入我们的团队，并期待他在与Celonauts同事们一起推动产品开发，为我们的客户提供变革性解决方案方面带来的重要贡献。他的专业知识将在确保我们继续领先市场、为客户提供无与伦比价值方面发挥关键作用。”
+
+Greg是分布式系统和组织扩展方面的专家。在Snowflake之前，他在Google工作了13年，担任工程副总裁，负责广泛的Google Cloud数据分析和机器学习产品，以及解决Google所有业务的数据分析需求的内部服务。他还在Sun Microsystems工作过六年，负责Java运行时环境和操作系统工作。
+
+Greg在康奈尔大学获得计算机科学博士学位，UC伯克利分校MBA学位，并获得AGH Krakow（波兰）的本科学位。他目前拥有50多项专利。
+
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
