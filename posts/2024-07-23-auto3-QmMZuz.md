017cda3083b22527cafa7afafe1ff6b924f68fd3
imjeson <jeson@imoocc.com>
2024-07-23 19:10:05 +0000

    Commit on Tue Jul 23 19:10:05 UTC 2024

---
+++ posts/2024-07-23-auto3-QmMZuz.md
@@ -0,0 +1,21 @@
+---
+title: 'Riveron收购Yantra以增强首席财务官的技术支持'
+date: 2024-07-24
+author: ByteAILab
+
+---
+
+Acquisition makes Riveron one of the largest NetSuite Alliance Partners.
+Riveron，一家由Kohlberg & Company支持的业务咨询公司，今天宣布收购了Yantra，一家拥有广泛专业知识的技术和咨询服务提供商，以满足技术化首席财务官迫切需要的需求。![图片](https://ai-techpark.com/wp-content/uploads/2024/07/Riveron-960x540.jpg){ width=60% }
+
+---
+此次收购使Riveron成为全球最大的NetSuite联盟合作伙伴之一，为客户提供全面解决方案以增强价值。Yantra还在数据科学和分析、人工智能（AI）、机器人流程自动化（RPA）和平台集成方面拥有重要专业知识。
+“具有前瞻性的组织需要能够适应和扩展的技术解决方案，无论业务环境如何，” Riveron首席执行官Sam Shaw表示。“通过Yantra，我们现在拥有全球团队，帮助Riveron为首席财务官办公室和私募股权公司提升绩效。”
+Yantra由Vikram Bhandari于2009年创立，员工约300人，分布在美国、加拿大、印度和菲律宾。Bhandari将加入Riveron担任首席技术和创新官，为多个领域的客户提供服务，包括对首席财务官如何实际应用人工智能的全新视角。通过收购，Riveron大幅扩展团队规模，全球专业人员总数增至1000余人。“多年来，Yantra帮助客户在不同的技术和业务领域实现数字转型，” Bhandari表示。“现在，作为Riveron的一部分，我们很高兴能在共同承诺增长和为客户提供下一代技术的道路上发挥关键作用。”
+Shaw补充道，“Yantra的传统深深扎根于推动创新。这一收购将巩固Riveron作为技术和变革领导者的地位，强化我们为客户提供价值的能力。展望未来，我们的联合优势将使我们能够为全球客户提供独特的竞争优势和持续成功。”
+交易条款未披露。Canaccord Genuity担任Riveron的财务顾问。Ropes & Gray担任Riveron的法律顾问。Telegraph Hill Advisors担任Yantra的财务顾问，Sklar Kirsh LLP担任Yantra的法律顾问。
+Explore AITechPark，获取人工智能、物联网、网络安全、AI技术新闻以及行业专家的深刻更新！
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
---
+++ posts/2024-07-23-auto3-ocCt9h.rb
@@ -0,0 +1,30 @@
+```
+title: 'Abridge、Mayo Clinic和Epic宣布合作'
+date: 2024-07-24
+author: ByteAILab
+
+---
+
+今天，Abridge、Mayo Clinic和Epic宣布为护士打造生成式人工智能环境文档工作流。![图片](https://ai-techpark.com/wp-content/uploads/2024/07/Abridge-May-960x540.jpg){ width=60% }
+
+---
+这次合作将使护士能够使用尖端解决方案。Abridge的新产品由Epic支持，将Abridge的生成式人工智能平台的优势与对现有Epic护理工作流的无缝集成结合在一起。
+
+作为Epic Workshop计划下一阶段合作的一部分，该计划旨在为医疗保健行业带来新的创新技术，Abridge、Epic和Mayo Clinic将与Mayo的护士合作，优先考虑该工具将产生最大影响的工作流。 Mayo的护士将在设计和测试此解决方案中发挥重要作用，该解决方案与Abridge面向医生的文档产品一起使用。
+
+“这次合作的核心是Mayo Clinic的护理人员，”梅奥诊所首席护理官Ryannon Frederick，M.S.，R.N.说。“我们直接让他们参与此技术的开发，以确保其使用满足护理和患者护理工作流程的独特需求以及环境解决方案的监管要求。我们很高兴能够借助我们护理人员的知识和专业技能来帮助塑造文档的未来，其中文档可以自动和有机地生成。”
+
+护理工作流程通常独特地包括快节奏、持续和广泛范围的活动，可能涉及多名患者和数百项任务。日常工作可能包括患者和团队之间的沟通、临床记事和数据采集。Abridge、Epic和Mayo Clinic共同努力确定了应用此技术的最高影响领域。
+
+“Abridge的人工智能技术、Epic的开发和Mayo Clinic的护理实践专业知识的结合努力和专业知识将帮助我们快速开发和迭代这项技术，以满足我们的护理人员和患者的需求，”梅奥诊所首席临床系统和信息官Edwina Bhaskaran，M.S.N.，R.N.说。“这项技术将通过每天为患者提供护理的护士的专业知识来塑造。我们的共同目标是在年底之前让工具到达我们护士手中。”
+
+“Abridge平台的扩展，具有专门的内部Epic住院工作流中的护理产品，将有助于解决全国范围内护士不断增加的行政负担问题，”Abridge的首席执行官兼创始人Shiv Rao博士说。
+
+“这项协作工作有可能重新思考护士如何在电子健康记录中记录对话，通过将患者和护士的声音直接录入记录，” Epic公司护理副总裁Emily Barey说。“我们知道每次对话对于获得最佳结果至关重要，我们对于作为值得信赖的助手的角色感到兴奋，可以将这些对话转化为行动，以便护士能够提供他们想要提供的护理。”
+
+此消息紧随在Abridge在University of Vermont Health Network、CHRISTUS Health、UChicago Medicine、Sutter Health、Yale New Haven Health System、UCI Health、Emory Healthcare、The University of Kans...
+
+--- 
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
