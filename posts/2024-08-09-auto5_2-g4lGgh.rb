0bf60bb1d3c8d5c07d311b13c3841756ab966a50
imjeson <jeson@imoocc.com>
2024-08-09 19:10:06 +0000

    Commit on Fri Aug  9 19:10:06 UTC 2024

---
+++ posts/2024-08-09-auto5_2-g4lGgh.rb
@@ -0,0 +1,119 @@
+```
+---
+
+title: '宁波东方理工大学 | 甬江论坛2024报名通道持续开放'
+date: 2024-08-10
+author: ByteAILab
+
+---
+
+宁波东方理工大学甬江论坛2024即将于**8月15日**截止申请，当前已收到来自海内外数百名优秀学者报名，欢迎感兴趣的海内外专家学者尽早申请！
+
+The application deadline of **August 15, 2024** for the Yongriver Forum 2024 @ Eastern Institute of Technology, Ningbo is approaching and we have received applications from hundreds outstanding scholars worldwide. We warmly welcome scholars interested in this forum to submit your application by August 15, 2024!
+
+![Image 1](https://image.jiqizhixin.com/uploads/editor/f9c4c2f9-7085-4499-bb05-4876a300794e/%E5%9B%BE%E7%89%871.png)
+
+**01** 论坛背景
+
+Background
+
+宁波东方理工大学（暂名）是一所由社会力量举办、国家重点支持、省市共同建设的小而精、高起点、高水平、国际化新型研究型大学，以服务国家发展、推动社会进步为使命，注重基础研究、突出前沿交叉、强化工程技术，坚持志存高远、求真务实的培养理念，培养具有宽厚基础、创新能力、家国情怀、国际视野的拔尖创新人才，以卓越的教育、科研和创新推动社会发展。
+
+东方理工将于**2024年8月31日-9月1日**以线上线下相结合形式，组织召开东方理工“甬江论坛2024”。
+
+---
+我们诚邀有意加盟东方理工的海内外优秀学者报名参加东方理工“甬江论坛2024”，分享各自领域的最新研究成果，促进多学科学术交流，期待与您携手在“书藏古今、港通天下”的历史文化名城宁波，共创一个世界级的教育机构与科研平台。
+
+Eastern Institute of Technology, Ningbo (EIT) cordially invites outstanding and enthusiastic scholars who are committed to excellence in research and teaching to participate in the 2024 Yongriver Forum. This forum aims to discuss pressing scientific and technological topics and facilitate the exchange of the latest research findings in a variety of scientific fields. Selected scholars will have the opportunity for onsite or online interviews for tenure-track or tenured faculty positions at EIT.
+
+![Image 2](https://image.jiqizhixin.com/uploads/editor/43a5a3cd-0dcd-4604-9833-a4881e266907/1723195032338.png)
+
+**02** 论坛学科领域
+
+Scope of the Forum
+
+宁波东方理工大学（暂名）组建理学、工学、信息、商科四大学科群，重点发展数学、物理、化学、生物、计算机等基础学科，以及集成电路、人工智能、智能制造、新能源、新材料、新医药等国家急需的前沿交叉学科，形成以理、工、信、商为主体，兼备特色人文社科的学科体系。
+
+东方理工实行国际学术机构通用的准聘-长聘（Tenure-track）制度，面向全球招聘助理教授、副教授、教授和讲席教授。当前招聘领域涵盖理学、工学、信息等学科群，包括但不限于下列方向：
+
+- **理学**：理论与应用数学、统计学与数据科学、物理学、化学、生命科学
+- **工学**：机械、力学、航空航天、机器人、智能制造、材料、能源、生物医学工程、土木、环境、海洋
+- **信息**：电子、微电子、集成电路、计算机、人工智能、软件工程、自动化控制、信息工程、通信工程
+
+The forum focuses on but not limited to the following scientific areas:
+
+- **Science**:
+  Basic and Applied Mathematics, Statistics and Data Science, Physics, Chemistry, and Life Science.
+- **Engineering**:
+  Mechanics and Mechanical Engineering, Robotics and Advanced Manufacturing, New Materials, Chemical and Molecular Engineering, Biomedical Engineering, Civil and Environmental Engineering, Ocean Engineering, Clean Energy and Sustainable Technologies.
+- **Information Science and Technology**:
+  Electronics, Microelectronics, Integrated Circuits, Computing, Artificial Intelligence, Software Engineering, Automation Control, Information and Communication Engineering.
+
+![Image 3](https://image.jiqizhixin.com/uploads/editor/11e63d9d-eb0d-4b6c-a744-12340a620fd8/1723195042524.png)
+
+**03** 申报条件
+
+Application Conditions
+
+申请人需在相关学科获得博士学位，在各自专业领域的顶级期刊上发表过高质量的学术论文，具有良好的中英文交流能力，并能胜任全英文教学工作。
+
+Applicants must have obtained a doctoral degree in relevant disciplines, have published high-quality academic papers in top journals in their respective professional fields, have good communication skills in Chinese and English, and be able to teach in English.
+
+**04 教研员工的薪酬待遇**
+
+Salary and Benefits
+
+宁波东方理工大学（暂名）为入职的教师提供具有全球竞争力的薪酬待遇和科研启动经费。生活配套包括住房津贴、子女就学机会、高端医疗保险、退休年金计划等全方位福利体系。
+
+EIT offers its faculty members globally competitive salary and benefit packages, including substantial startup fundings for research equipment acquisition, laboratory technicians, and graduate students and postdocs. Comprehensive health insurance, retirement benefits and relocation assistance are also offered to meet individual circumstances and needs.
+
+![Image 4](https://image.jiqizhixin.com/uploads/editor/39cd824e-6fe6-41ad-8343-adf6b338ee5f/1723195053676.png)
+
+**05** 论坛申请流程
+
+Application Procedure
+
+申请人请**扫描文末二维码**填写相关信息并上传附件。申请截止日期为8月15日。邀请参加甬江论坛的正式通知信将于8月22日前陆续发出。
+
+**申请材料：**
+- 个人简历（包括完整的学术论文列表）
+- 研究兴趣陈述
+- 教学理念陈述
+- 5 篇近期代表性论文全文复印件
+- 3-5 位推荐人姓名、工作单位与联系方式
+
+**咨询电话：**
+- 理学部： 胡老师 0574-86603206
+- 工学部： 叶老师 0574-86603220
+- 信息学部： 杨老师 0574-86603232
+- 数字孪生院：刘老师 0574-86578196
+
+Please **scan the QR code below** to submit your application. The deadline for submission is August 15, 2024. Shortlisted candidates will receive invitation to participate in the Yongriver Forum by August 22, 2024. Please include the following materials in your application:
+- Curriculum vitae (CV) including a full list of publications.
+- Copy of five of your selected publications.
+- Research Statement outlining your current and future research agenda.
+- Teaching Statement outlining your teaching experience and philosophy.
+- Contact information of 3 professional references.
+For further information, please reach out to the respective Colleges/Institute.
+
+- **College of Science**
+  Hu Qiong: +86-574-86603206
+- **College of Engineering**
+  Ye Yijing: +86-574-86603220
+- **College of Information Science and Technology**
+  Yang Fan: +86-574-86603232
+- **Ningbo Institute of Digital Twin (IDT)**
+  Liu Chang: +86-574-86578196
+
+![Image 5](https://image.jiqizhixin.com/uploads/editor/ed66fb93-926f-441c-a8f9-d483fef56d12/1723195075803.png)
+
+https://www.wjx.top/vm/PpBNfVm.aspx
+
+申请人请扫描**二维码**或复制链接至**浏览器**填写相关信息并上传附件。申请截止日期为8月15日。邀请参加甬江论坛的正式通知信将于8月22日前陆续发出。本次招聘中符合要求的简历也有机会受邀面试宁波数字孪生（东方理工）研究院的工作岗位，具体岗位内容请点击阅读：https://mp.weixin.qq.com/s/FZ3iVjQhnGTYjKC1U1QZkg
+
+To apply, please scan the QR code or open the link in your browser, then fill in your relevant information and upload attachments. The application deadline is August 15, 2024. Formal invitation letters for attending the forum will be sent out by August 22, 2024. Selected applicants will have the opportunity to be interviewed for tenure-track and tenured faculty positions at EIT. For more information, please click: https://mp.weixin.qq.com/s/FZ3iVjQhnGTYjKC1U1QZkg.
+
+---
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
