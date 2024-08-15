044e9fd7d78dd0a7f763d9d45d58d7f6b2ea88fc
imjeson <jeson@imoocc.com>
2024-08-15 23:10:05 +0000

    Commit on Thu Aug 15 23:10:05 UTC 2024

---
+++ posts/2024-08-15-auto4-xlcqoc.rb
@@ -0,0 +1,20 @@
+
+
+---
+---
+
+title: 'AI大航海时代，再看不懂PCB真要落伍了'
+date: 2024-08-16
+author: ByteAILab
+
+---
+
+初识PCB：电子产品之母，你很难不动心
+1）有电的地方就有PCB
+Printed Circuit Board，缩写为大家熟知的PCB，又被称为印刷电路板，因其制造采取“电子印刷”工艺而得名。
+PCB的作用，是充当电子元器件进行电气互连的载体与起到支撑作用的基座，不仅为线路提供电绝缘等电气阻抗特性，还承载着电子设备的数字/模拟信号传输...
+
+...
+稿结束，查看原文：[https://www.aixinzhijie.com/article/6846432](https://www.aixinzhijie.com/article/6846432)
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
