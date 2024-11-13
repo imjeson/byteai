fc97ab3d2f25f3ead6a660d92d7c638f6727f22c
imjeson <jeson@imoocc.com>
2024-11-13 19:10:04 +0000

    Commit on Wed Nov 13 19:10:04 UTC 2024

---
+++ posts/2024-11-13-auto3-phUH9j.rb
@@ -0,0 +1,23 @@
+```
+---
+
+title: '财务主管Ravi Narula被任命为SymphonyAI的首席财务官'
+date: 2024-11-14
+author: ByteAILab
+
+---
+
+老手Ravi Narula加入公司担任首席财务官，他为公司的财务规划和运营带来了增长型的思维方式
+SymphonyAI是企业预测和生成AI SaaS产品领域的领导者，今天宣布行业资深人士Ravi Narula已加入公司担任首席财务官。![图片](https://ai-techpark.com/wp-content/uploads/2024/11/Finance-960x540.jpg){ width=60% }
+
+---
+凭借操控财务战略、运营卓越以及创造价值的可靠记录，Narula加入SymphonyAI旨在支持公司持续快速增长。
+Narula将负责SymphonyAI的财务职能，包括会计、审计、企业金融、税务、财务和投资者关系活动，他将与SymphonyAI业务领导合作，进一步执行SymphonyAI的盈利增长战略。
+“我欢迎Ravi加入SymphonyAI领导团队。他在帮助将科技初创公司转型为成熟上市公司方面的记录将有助于我们达到下一个业务增长和执行卓越水平，”SymphonyAI CEO Sanjay Dhawan表示。“正值SymphonyAI重新聚焦执行盈利增长轨迹的重要时刻，Ravi在引导高科技增长公司实现关键企业里程碑方面的经验至关重要。”
+“加入SymphonyAI这样一个AI行业领导者，提供可衡量的业务成果的预测和生成AI垂直产品，令人兴奋，”Narula说道。“我很荣幸能在增长动能不断加强的时刻加入一个世界一流团队，帮助推动公司达到下一个操作稳健水平。我重视SymphonyAI在商业AI创新、增长、敏捷性和卓越方面所享有的当之无愧声誉。”
+Narula的职业生涯超过25年，涉及财务管理、业务领导和公司战略，包括在高科技SaaS公司和Deloitte担任高管职务。Narula曾担任过多家上市公司的首席财务官，包括Ooma（纽交所：OOMA）、Gigamon（纽交所：GIMO）和BigBand Networks，在这些公司中，他领导了各自的首次公开募股，还曾在私营公司如Aura和Certinia担任职务。
+
+---
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
