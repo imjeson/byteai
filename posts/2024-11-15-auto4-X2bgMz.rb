1b8f76414a3a59300a077ebc9b3b7fdb8e2cb5bd
imjeson <jeson@imoocc.com>
2024-11-15 05:40:05 +0000

    Commit on Fri Nov 15 05:40:04 UTC 2024

---
+++ posts/2024-11-15-auto4-X2bgMz.rb
@@ -0,0 +1,81 @@
+---
+
+title: '刚刚，OpenAI发布Windows版ChatGPT，高级语音能用了'
+date: 2024-11-16
+author: ByteAILab
+
+---
+
+<div class="ne-atomic">
+  <div class="ne-img-wrapper" data-align="c">
+    <img alt="图片来源：由GPTNB生成" src="http://www.jesonc.com/upload/8FD7B96F5E34993C64020C0DB54F4C00/1731638916356/FsKTSEOebAQQgTvKBe1eTVje384y.png" style="width:100%">
+    <div class="ne-img-desc">图片来源：由GPTNB生成</div>
+  </div>
+</div>
+
+今天凌晨2点，OpenAI正式向所有Windows用户开放桌面版ChatGPT。
+
+桌面版ChatGPT支持拍照识别和高级语音两大特色模式，例如，你可以拍摄一幅山水画，然后基于这幅画进行各种提问。
+
+---
+
+
+OpenAI之前发布的GPT-4o的高级语音模式，也可以在桌面版ChatGPT使用了，你可以像和老朋友聊天一样，实时打断与ChatGPT对话。
+
+这对于教育领域帮助巨大，可以用它练口语、解读数学、物理等各种难题，每一个人都能拥有自己的“贾维斯”。
+
+<div class="ne-atomic">
+  <div class="ne-img-wrapper" data-align="c">
+    <img alt="" src="http://www.jesonc.com/FgyEyKsLFzlyhQesVqmSx6ozMwgx" style="width:100%">
+  </div>
+</div>
+
+网友表示，非常棒的消息！现在是时候提高高级语音聊天的限制了——这将帮助我们许多人更高效地工作。
+
+<div class="ne-atomic">
+  <div class="ne-img-wrapper" data-align="c">
+    <img alt="" src="http://www.jesonc.com/FtomtLhOGGyQ4OAxGcw_MSAd2INL" style="width:100%">
+  </div>
+</div>
+
+这真是个突破！Alt + Space 快捷键肯定更方便。高级语音模式听起来也是一种很酷的多任务处理方式！
+
+...
+
+<div class="ne-atomic">
+  <div class="ne-img-wrapper" data-align="c">
+    <img alt="" src="http://www.jesonc.com/Fp5A5qw9cyfEEWvN90qKpfehbusC" style="width:100%">
+  </div>
+</div>
+
+好家伙，还有人想要XP版本，这老古董。Windows10都得高级版。
+
+<div class="ne-atomic">
+  <div class="ne-img-wrapper" data-align="c">
+    <img alt="" src="http://www.jesonc.com/FhxkPUHIs6kG_qKMOxbb3SNdH5zD" style="width:100%">
+  </div>
+</div>
+
+下载完Windows版ChatGPT客户端之后，启用方法很简单，使用快捷键Alt + Space就能访问了。
+
+你可以使用PC自带的摄像头进行拍照，然后让ChatGPT进行解读或提问。
+
+...
+
+<div class="ne-atomic">
+  <div class="ne-img-wrapper" data-align="c">
+    <img alt="" src="http://www.jesonc.com/FsSUfxaHEwUbARfXsD8gKt8gG811" style="width:100%">
+  </div>
+</div>
+
+桌面版ChatGPT提供了伴侣功能，可以在不同的应用中出现方便使用。伴侣聊天窗口会记住它最后的位置，并在应用重置时自动回到屏幕底部中心，这样可以减少用户的调整工作，提高使用效率。
+
+在伴侣聊天窗口中，你可以随时通过点击窗口顶部的“在主窗口中打开”按钮，将对话转移到ChatGPT的主应用窗口中继续。
+
+如果不小心关闭了伴侣聊天窗口，你可以通过访问ChatGPT应用侧边栏的聊天历史来继续对话，确保不会丢失任何聊天内容。
+
+目前，所有用户都能使用Windows桌面版本了，去微软的应用商店就能快速获取。 
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
---
+++ posts/2024-11-15-auto5-SU3RbS.rb
@@ -0,0 +1,33 @@
+```
+---
+
+title: '穹彻智能-上交大最新Nature子刊速递：解析深度学习驱动的视触觉动态重建方案'
+date: 2024-11-16
+author: ByteAILab
+
+---
+
+随着人形机器人技术的迅猛发展，如何有效获取高质量的操作数据成为核心挑战。鉴于人类操作行为的复杂性和多样性，如何从真实世界中精准捕捉手与物体交互的完整状态，成为推动人形机器人操作技能学习的关键所在。
+
+---
+面对这一挑战，穹彻智能携手上海交通大学卢策吾和刘景全团队，创新性地提出了名为 ViTaM 的视觉-触觉联合记录和追踪系统。该系统包括高密度可伸缩触觉手套和基于视觉-触觉的联合学习框架，不仅在触觉手套的设计和制造上实现了技术突破，更通过视觉与触觉的深度融合，为理解手物...
+
+https://image.jiqizhixin.com/uploads/editor/f0c2d606-4c04-4f9c-81b6-d7741aafa185/640.png
+
+**论文名称**：Capturing forceful interaction with deformable objects using a deep learning-powered stretchable tactile array
+
+**论文链接**：https://www.nature.com/articles/s41467-024-53654-y
+
+**项目地址**：https://github.com/jeffsonyu/ViTaM
+
+**演示视频**：
+- [视频1](https://mp.weixin.qq.com/s?__biz=MzA3MzI4MjgzMw==&mid=2650942987&idx=2&sn=3d32dfd94578c347c15bb89bb3a47dd6&chksm=84e7ea75b3906363c613ac948b5661b628d3569f9af5dcfc62a4a0b45ea10177de9a6b43afcd&token=319039997&lang=zh_CN#rd)
+- [视频2](https://mp.weixin.qq.com/s?__biz=MzA3MzI4MjgzMw==&mid=2650942987&idx=2&sn=3d32dfd94578c347c15bb89bb3a47dd6&chksm=84e7ea75b3906363c613ac948b5661b628d3569f9af5dcfc62a4a0b45ea10177de9a6b43afcd&token=319039997&lang=zh_CN#rd)
+- [视频3](https://mp.weixin.qq.com/s?__biz=MzA3MzI4MjgzMw==&mid=2650942987&idx=2&sn=3d32dfd94578c347c15bb89bb3a47dd6&chksm=84e7ea75b3906363c613ac948b5661b628d3569f9af5dcfc62a4a0b45ea10177de9a6b43afcd&token=319039997&lang=zh_CN#rd)
+- [视频4](https://mp.weixin.qq.com/s?__biz=MzA3MzI4MjgzMw==&mid=2650942987&idx=2&sn=3d32dfd94578c347c15bb...<!-- truncated content -->
+
+---
+
+```
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
