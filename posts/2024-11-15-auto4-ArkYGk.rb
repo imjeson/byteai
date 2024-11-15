44d4c67a2661bf1942b3205ceb9b1c14069cc813
imjeson <jeson@imoocc.com>
2024-11-15 23:10:05 +0000

    Commit on Fri Nov 15 23:10:05 UTC 2024

---
+++ posts/2024-11-15-auto4-ArkYGk.rb
@@ -0,0 +1,17 @@
+---
+title: 'Scaling Law遭遇瓶颈，OpenAI被曝押注智能体“Operator”'
+date: 2024-11-16
+author: ByteAILab
+
+---
+
+<div class="article-content__container"><div class="bbt-html"><div class="ne-render-content"><p>文章来源：<a href="https://mp.weixin.qq.com/s/s0LBjZ0AwI7t46hmrX7yAQ" rel="noopener nofollow" target="_blank">量子位</a></p><div class="ne-atomic"><div class="ne-img-wrapper" data-align="c"><img alt="图片来源：由GPTNB生成" src="http://www.jesonc.com/upload/8FD7B96F5E34993C64020C0DB54F4C00/1731651270358/FneCl_7FW1aQE69M_zsBfrESaiA_.png" style="width:100%"><div class="ne-img-desc">图片来源：由GPTNB生成</div></img></div></div><p>继Anthropic之后，OpenAI也要接管人类电脑了？！</p><p>就在刚刚，彭博社爆料OpenAI将在<strong>明年1月</strong>推出Agent<strong>“Operator</strong>（操作员）<strong>”</strong>，为用户自动执行任务。</p><div class="ne-atomic"><div class="ne-img-wrapper" data-align="c"><img alt="" src="http://www.jesonc.com/FlTIIba0WZpY2wJX7zdRahADxDUa" style="width:100%"/></div></div><p>配方我们也很熟悉，只需在电脑上简单下达指令，Agent就能自动帮我们编码开发应用、订餐，做攻略等等。
+
+---
+</p><p>好嘛，一看大家就明白，这是要和老对手Anthropic打起来了！</p><blockquote>这将与Anthropic的computer use API进行竞争（Anthropic之前发布了computer use功能，号称接管人类电脑）</blockquote><p class="ne-empty-p"><br/></p><div class="ne-atomic"><div class="ne-img-wrapper" data-align="c"><img alt="" src="http://www.jesonc.com/Fh0TfE0iahH0QnuQ-6mFPunftS2v" style="width:100%"/></div></div><p>事实上，在前一阵Reddit举办的有问必答活动上，CEO奥特曼曾信誓旦旦表示：</p><blockquote>Agent将成为下一个重大突破。</blockquote><p>同时，OpenAI首席产品官Kevin Weil进一步表示，<strong>让ChatGPT自主执行任务将是明年一大重点。</strong></p><div class="ne-atomic"><div class="ne-img-wrapper" data-align="c"><img alt="" src="http://www.jesonc.com/Fr50hkgESYh1kF2EIodzuL69d23p" style="width:100%"/></div></div><p>不过，除了“遥遥领先”的Anthropic，OpenAI另一对手谷歌早已曝出将于12月发布自己的AI Agent版本。</p><p>国内百度智谱等模型厂商也推出了类似智能体和解决方案，甚至手机厂商荣耀、小米等还发布了自己的AI操作系统。</p><p>如此一来，压力也是来到了OpenAI这边。</p><p>...
+
+---
+
+### Please note that the content has been truncated for brevity.
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
