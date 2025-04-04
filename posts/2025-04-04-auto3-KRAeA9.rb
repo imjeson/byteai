2dc1cebb543e797187d7435067d43cd57cf8ed55
web3nb <jesonchen@imoocc.com>
2025-04-04 19:10:05 +0000

    Commit on Fri Apr  4 19:10:05 UTC 2025

---
+++ posts/2025-04-04-auto3-KRAeA9.rb
@@ -0,0 +1,24 @@
+title: 'Beyond Identity任命新领导以推动战略增长举措'
+date: 2025-04-05
+author: ByteAILab
+
+---
+
+Ligeia Zeruto、Bob Burke 和 Louis Marascio 被任命为 Beyond Identity 的高管团队，以通过联邦业务、安全设计计划和产品创新加速增长
+Beyond Identity 是一家领先的安全身份和访问管理（IAM）解决方案提供商，旨在消除基于身份的攻击。![图片](https://ai-techpark.com/wp-content/uploads/Beyond-Identity.jpg){ width=60% }
+
+---
+该公司欣然宣布任命三位杰出的专业人士加入其高管团队，进一步巩固公司在技术创新和网络安全领导力方面的承诺。
+Ligeia Zeruto 担任联邦、国防和国际扩展负责人
+Ligeia Zeruto 加入 Beyond Identity，担任联邦与国防负责人，带来了来自公共和私营部门的丰富经验，并在 Zeruto 女士的领导下建立了公司的联邦业务。 在此任命之前，Zeruto 女士曾在亚马逊网络服务（AWS）担任高管，包括公共部门的首席幕僚和全球高级技术合作伙伴销售经理。此外，她的军旅生涯也非常辉煌，曾在美国陆军担任23年的反情报和网络战军官，其中包括在美国网络司令部、国家安全局和第75创新指挥部的职责。
+“Ligeia的任命及其在支持国家网络安全战略发展方面的专业知识与我们的战略目标相一致，旨在扩大我们在联邦和国防领域的影响力，”Beyond Identity首席执行官兼联合创始人Jasson Casey表示。“她的见解和经验在满足联邦机构独特需求的市场推广中将是无价的。”
+Bob Burke 晋升为首席信息安全官
+Bob Burke 在在担任安全和基础设施副总裁期间的突出表现后，晋升为 Beyond Identity 的首席信息安全官。在他的前任职务中，Burke 负责监督所有产品和业务线的基础设施、云操作和安全。他作为安全从业者和产品架构师的双重角色确保了 Beyond Identity 内部企业系统和 SaaS 产品的可用性、性能、可扩展性和安全性。
+“Bob 升任 CISO 进一步强调了我们对保持最高安全标准的承诺，”Casey 评论道。“他的领导将确保我们继续保护客户免受快速演变的网络威胁环境的影响。”
+Louis Marascio 晋升为首席技术官
+Louis Marascio 在量化交易、IP 电话、数据分析、制造业和网络安全等各个行业拥有超过 25 年的经验，致力于打造公司、团队和产品。在晋升为首席技术官之前，Marascio 在 Beyond Identity 担任高级产品架构师，发挥了关键作用，实现了公司保护客户生计的使命。
+“Louis 的丰富背景和创新思维为提升我们的技术能力发挥了重要作用，”Casey 表示。“作为 CTO，他的领导对于推动我们的产品开发和技术战略至关重要。”
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
