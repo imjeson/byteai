ff9729608e4e5163afcb3350435088368c791c74
web3nb <jesonchen@imoocc.com>
2025-04-06 19:10:07 +0000

    Commit on Sun Apr  6 19:10:07 UTC 2025

---
+++ posts/2025-04-06-auto3-NPdAqM.rb
@@ -0,0 +1,26 @@
+title: 'Hornetsecurity集团收购Altospam'
+date: 2025-04-07
+author: ByteAILab
+
+---
+
+Hornetsecurity进一步巩固其作为欧洲网络安全冠军的地位。![图片](https://ai-techpark.com/wp-content/uploads/Hornetsecurity-Group.jpg){ width=60% }
+
+---
+Hornetsecurity是一家全球领先的下一代云安全、合规、备份和安全意识解决方案提供商，今天宣布已签署一项最终协议，收购法国电子邮件网络安全公司Altospam。此项公告是在法国的INCYBER欧洲论坛（FIC）上发布的，Hornetsecurity在这里占用了1800平方米的Zenith de Lille场地，为与会者提供了一次身临其境的体验。
+
+此次收购的完成仍需法国经济部在法国外商直接投资法规下的批准。此次收购将补充Hornetsecurity的战略，即为全球企业和组织提供由欧洲构建的网络安全解决方案，并进一步扩大该集团在法国的专业知识和业务据点，此前Hornetsecurity于去年收购了法国的Vade Secure。
+
+总部位于德国汉诺威的Hornetsecurity因其单一平台方法而闻名于国际，为Microsoft 365提供综合解决方案，包括网络安全、备份和GRC以及安全意识，而总部位于法国波尔多的Altospam以其在电子邮件网络安全领域的专业知识而受到认可，提供保护专业电子邮件的解决方案。
+
+"我们很高兴加入Hornetsecurity集团，这使我们能够加入一个卓越的欧洲网络安全冠军，而目前的网络安全世界大多以美国为中心，" Altospam首席执行官Jean-Christian Dumas表示。"这一重要里程碑将为我们的合作伙伴和客户提供更广泛的健壮的AI驱动的网络安全解决方案，这些解决方案均在欧洲自豪地构建，将超越电子邮件安全，涵盖备份与恢复、权限管理、合规和安全意识。"
+
+"Hornetsecurity集团欢迎Altospam的加入，我们对此感到非常高兴，" Hornetsecurity首席执行官Daniel Hofmann表示。"作为一家受人尊敬的欧洲开发商，Altospam的核心目标与Hornetsecurity创建更安全世界的使命紧密相关，通过我们的下一代云基础网络安全服务，实现整体协同效应，造福合作伙伴和客户。"
+
+他补充道："作为我们泛欧战略的一部分，我们确保客户数据在各自国家的边界内保持安全，保障数据主权、安全和隐私。" 
+
+此交易符合Hornetsecurity的产品建设和国际扩张战略。Hornetsecurity在PSG Equity、TA Associates和Verdane三家领先的软件和专业增长投资公司的支持下，旨在确立其作为国际云安全与合规软件冠军的地位。
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
