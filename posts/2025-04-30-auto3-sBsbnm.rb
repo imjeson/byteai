5158e51ff29263b241f7e4cbfd25db4460817bcc
web3nb <jesonchen@imoocc.com>
2025-04-30 19:10:06 +0000

    Commit on Wed Apr 30 19:10:05 UTC 2025

---
+++ posts/2025-04-30-auto3-sBsbnm.rb
@@ -0,0 +1,22 @@
+title: 'Forward Networks连续第三年获得全球信息安全大奖'
+date: 2025-05-01
+author: ByteAILab
+
+---
+
+Forward Networks在2025年RSAC上荣获第13届全球信息安全大奖中的“开拓者合规性”奖
+
+Forward Networks今天宣布，被评为Cyber Defense Magazine第13届全球信息安全大奖的“开拓者合规性”奖获奖者。![图片](https://ai-techpark.com/wp-content/uploads/Forward-Networks.jpg){ width=60% }
+
+---
+此次奖项的获得标志着Forward连续第三年获得全球信息安全奖，此前在2024年获得“脆弱性评估、补救和管理市场领袖”奖和在2023年获得“云安全热公司”奖。
+
+Forward Enterprise是唯一一个能够为整个网络提供始终准确的合规状态的网络数字双胞胎，并附带优先补救计划，解决在数据收集过程中识别的任何问题。Forward Enterprise从网络中的所有L2-L7设备（本地和云）收集配置和状态数据。这些数据形成了准确的网络资产清单和拓扑，作为其开创性合规功能的基础，这些功能包括持续网络安全合规监测、CVE（常见漏洞和暴露）补救以及零信任态势验证。
+
+“随着攻击面演变和新威胁的出现，安全合规性的重要性持续增长。在Forward Enterprise，我们的目标是通过提供准确、最新的合规视图，以及在需要时采取明确、可操作的见解，来减少不确定性，从而保护网络。” Forward Networks的产品与用户体验高级副总裁Chiara Regale表示。“我们专注于帮助安全团队摆脱耗时的手动过程，同时提供前所未有的安全态势洞察。连续第三年被认可为全球信息安全奖获奖者，确实是一种荣誉，也是对我们团队奉献精神的证明。”
+
+“我们在全球范围内寻找能够带来巨大变化并可能帮助改变对抗网络犯罪激增的趋势的网络安全创新者。Forward Networks绝对值得获得这一备受追捧的奖项，并值得在你的环境中部署。” Cyber Defense Magazine的全球编辑Yan Ross说道。
+
+PR Newswire PR Newswire赋能沟通者识别和接触关键影响者，策划和分发有意义的故事，并衡量其努力的财务影响。Cision是全球领先的公关和市场传播专业人士提供的 earned media 软件和服务供应商。
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
