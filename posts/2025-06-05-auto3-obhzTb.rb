d03a0421175d3a3c3caa789ba2520445482ffa60
web3nb <jesonchen@imoocc.com>
2025-06-05 19:10:06 +0000

    Commit on Thu Jun  5 19:10:06 UTC 2025

---
+++ posts/2025-06-05-auto3-obhzTb.rb
@@ -0,0 +1,32 @@
+title: '新Kyndryl-Databricks合作推动客户数字化转型'
+date: 2025-06-06
+author: ByteAILab
+
+---
+
+合作将利用Kyndryl的AI服务支持客户采用Databricks数据智能平台
+
+Kyndryl（NYSE: KD），一家领先的关键任务企业技术服务提供商，今天宣布与数据和AI公司Databricks建立全球战略伙伴关系，以通过在规模上实现和交付AI，使客户实现数字化转型，并成为数据驱动型组织。![图片](https://ai-techpark.com/wp-content/uploads/New-Kyndryl.jpg){ width=60% }
+
+---
+
+
+通过将Kyndryl的数据和人工智能（AI）服务与Databricks数据智能平台结合，后者是一个统一的数据、分析和AI平台，双方合作进一步帮助客户现代化其IT生态系统，并以有成本效益的方式实现其业务目标。
+
+Kyndryl全球战略联盟领导Giovanni Carraro表示：“与Databricks的合作进一步提升了我们支持客户和推动业务增长的能力，通过建立正确的数字基础，并提供完整的应用及数据现代化服务。我们将通过提供企业实现规模化AI所需的集成解决方案来扩大Kyndryl服务的覆盖范围，以便客户能够从数字化转型旅程中获益。”
+
+Databricks咨询与解决方案集成副总裁Jason McIntyre表示：“随着企业对数据智能的需求不断增长，Kyndryl的端到端服务、管理和现代化客户IT环境的经验，以及稳固的客户关系使其成为推动客户采用Databricks的重要合作伙伴。我们期待支持我们的共同客户现代化其数据并实现业务目标。”
+
+根据该联盟，Kyndryl将提供其全面的数据、AI和现代化服务，支持客户利用Databricks数据智能平台，包括：
+
+建立和现代化数据基础：Kyndryl将提供端到端的数据服务，以帮助客户采用Databricks并现代化其数据及数据架构，同时确保数据质量、安全性和可扩展性。
+
+加速AI采用：Kyndryl将指导客户应用Databricks平台，利用机器学习、自然语言处理和计算机视觉等高级功能来挖掘数据潜力，获得更深层次的洞察，并实现规模化AI。
+
+整合SAP数据：Kyndryl将支持客户通过SAP Business Data Cloud和SAP Databricks整合其SAP和企业数据，以解锁洞察、创造商业价值并提高生产力。
+
+提供分析洞察：Kyndryl将结合其深厚的主机经验与Databricks，为传统系统与尖端分析之间架起桥梁，从而为客户提供更多的商业洞察和价值。
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
