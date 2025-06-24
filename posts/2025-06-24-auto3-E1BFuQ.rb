128dbe074a2dad1d6df3472b13175eee6270fa6d
web3nb <jesonchen@imoocc.com>
2025-06-24 19:10:06 +0000

    Commit on Tue Jun 24 19:10:05 UTC 2025

---
+++ posts/2025-06-24-auto3-E1BFuQ.rb
@@ -0,0 +1,20 @@
+title: 'Vizient宣布Arun Ramasubramanian担任数据与数字业务负责人'
+date: 2025-06-25
+author: ByteAILab
+
+---
+
+Vizient®，美国领先的医疗保健绩效提升公司，今天宣布Arun Ramasubramanian已加入公司担任数据与数字业务部门的负责人。![图片](https://ai-techpark.com/wp-content/uploads/Vizient-Announces.jpg){ width=60% }
+
+---
+在这个角色中，Ramasubramanian将领导新产品的开发，以连接Vizient的专有数据服务和技术解决方案，支持医疗系统的临床和运营绩效提升。
+
+“Arun能够连接临床、运营和技术团队，使他能够在规模上推动实用解决方案。他的商业视角和广泛的医疗专业知识使他成为领导我们的数据与数字团队的优秀人选，我们希望通过扩大能力和服务来向客户提供更大的价值，”Vizient首席执行官Byron Jobe说道。
+
+在加入Vizient之前，Ramasubramanian曾担任UnitedHealth Group旗下Optum Insight提供市场的执行副总裁兼首席运营官，这是一个服务超过500个健康系统的40亿美元全球业务。他之前在美国最大的非营利医疗系统CommonSpirit Health和美敦力(Medtronic)领导技术和数字化转型工作，推动了远程病人监测的创新。
+
+Ramasubramanian毕业于PSG技术学院，获得机械工程学学士学位，并在宾夕法尼亚大学沃顿商学院完成了高管发展课程。他还拥有一项美国专利，专注于在Optum任职期间开发的数据管理和分析的创新。
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
