c336c3d7c615585d4b1768b6f60de7314e155b84
web3nb <jesonchen@imoocc.com>
2025-07-25 19:10:05 +0000

    Commit on Fri Jul 25 19:10:05 UTC 2025

---
+++ posts/2025-07-25-auto3-R0PYm8.rb
@@ -0,0 +1,27 @@
+title: 'RiverMeadow 实现安全工作负载迁移至 AWS Outposts'
+date: 2025-07-26
+author: ByteAILab
+
+---
+
+RiverMeadow Software，一家领先的多云迁移和现代化解决方案提供商，今天宣布增强对 AWS Outposts 的支持，使组织能够安全地迁移和现代化本地工作负载至 AWS 管理的基础设施。![图片](https://ai-techpark.com/wp-content/uploads/RiverMeadow.jpg){ width=60% }
+
+---
+
+随着组织寻求降低许可成本并加速创新，许多人正在从 VMware vSphere 等遗留平台迁移。然而，关于数据驻留、延迟和隐私的严格要求常常阻碍迁移到公共云。AWS Outposts 提供了一种混合云解决方案，允许组织在本地（本地设备上）运行 AWS 服务，同时与更广泛的 AWS 生态系统保持无缝集成。
+RiverMeadow 的工作负载迁移平台现在支持向 AWS Outposts Racks 进行端到端的工作负载迁移，为客户提供了一条高度安全和高效的现代化路径，而不会牺牲合规性或控制权。
+“通过 RiverMeadow 的本地私有部署，组织可以在数据从未离开本地环境的情况下，将工作负载迁移到 AWS Outposts，” RiverMeadow 首席执行官 Jim Jordan 说。“我们的直接源到目标传输过程确保在整个迁移过程中提供最高水平的数据安全性和效率。”
+
+关键能力和好处：
+
+- 私有平台部署：RiverMeadow 直接在 AWS Outposts 上安装，消除了数据出口的需求，并支持严格的监管要求。
+- 直接数据传输：数据从源安全地移动到 Outposts 上的目标 EC2 实例，无需在 AWS 区域进行中转。
+- 操作系统现代化：在迁移过程中升级 Windows Server 和 Linux 操作系统，以减少技术债务，满足合规标准。
+- 存储权重调整：通过在迁移过程中动态调整存储大小，优化 AWS Outposts 的存储使用。
+
+RiverMeadow 对 AWS Outposts 的支持非常适合希望在本地保留敏感工作负载，同时远离成本高昂的遗留平台的组织。通过结合 AWS Outposts 的强大功能与 RiverMeadow 的自动化和智能，客户获得了一次无缝、安全且现代化的迁移体验。
+欲了解有关 RiverMeadow AWS Outposts 功能的更多信息或安排演示，请访问 www.rivermeadow.com。
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
