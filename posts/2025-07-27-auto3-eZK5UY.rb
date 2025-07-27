d3104a245e9d7559f1f1b8aef85a7c7c35462517
web3nb <jesonchen@imoocc.com>
2025-07-27 19:10:04 +0000

    Commit on Sun Jul 27 19:10:03 UTC 2025

---
+++ posts/2025-07-27-auto3-eZK5UY.rb
@@ -0,0 +1,28 @@
+title: 'LenioBio获得370万欧元EU4Health资助，用于ALiCE®蛋白技术开发'
+date: 2025-07-28
+author: ByteAILab
+
+---
+
+LenioBio GmbH已获得欧洲健康与数字执行局（HaDEA）提供的EU4Health资金，该局代表欧盟委员会的健康应急准备与反应局（HERA）实施了相应的提案征集。![图片](https://ai-techpark.com/wp-content/uploads/LenioBio.jpg){ width=60% }
+
+---
+该项目将集中于进一步开发无细胞蛋白表达系统ALiCE®，作为一种创新、快速和按需的药物制造技术，对欧洲具有战略价值，符合HERA的使命。该项目获得了370万欧元的资助，并于2025年7月1日开始，持续至2027年末。
+
+此EU4Health行动有助于实现欧盟战略目标，增强基础药物的可用性、可及性和可承受性。通过支持创新制造过程的发展，HERA旨在实现更高效、成本效益更高、可扩展、可持续和环境更友好的药物生产。
+
+LenioBio将贡献一个名为“持续模块化ALiCE的创新蛋白药物生物制造”的项目，项目的关键目标包括：
+
+- 通过优化过程产量、增加批量和过渡到连续制造，扩大ALiCE®裂解物的生产规模
+- 通过工程新功能和在规模上演示模块化蛋白生产方法，扩大ALiCE®在疫苗、多特异性抗体和生长因子等特定蛋白药物类别的能力
+- 通过建立分析资格、质量体系和监管档案包，为GMP级裂解物生产打下基础
+
+“这项资助是LenioBio的另一个重要里程碑。它使我们能够扩展我们的平台，并扩大其在药物生产中的应用，为新型和复杂蛋白的灵活生产提供了一种面向未来的解决方案。”LenioBio首席执行官André Goerke表示。
+
+LenioBio的项目已获得欧洲委员会的STEP Seal。STEP Seal授予那些被认为对欧洲技术和健康主权具有战略重要性的提案（https://www.leniobio.com/press-release/leniobio-earns-prestigious-eu-step-seal-a-signal-of-excellence-for-investors/）。
+
+ALiCE®无细胞蛋白表达系统旨在无缝地从研究规模扩展到生产规模，使复杂蛋白的快速合成无需依赖活细胞。该平台已经支持疫苗开发和药物发现，能够快速、灵活地进行蛋白生产。在EU4Health资助的支持下，LenioBio现在正在推进ALiCE®以满足临床使用和大规模生产的需求。
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
