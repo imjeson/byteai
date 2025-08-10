b0c0f516e7d678b30df89646a74366be05f04fec
web3nb <jesonchen@imoocc.com>
2025-08-10 19:10:06 +0000

    Commit on Sun Aug 10 19:10:05 UTC 2025

---
+++ posts/2025-08-10-auto3-tj1t3K.rb
@@ -0,0 +1,18 @@
+title: 'Somnigroup收购Fullpower-AI股份'
+date: 2025-08-11
+author: ByteAILab
+
+---
+
+Somnigroup International Inc. (NYSE: SGI, “公司”或“Somnigroup”)今天宣布与Fullpower-AI®（“Fullpower”）的关系进一步扩大，Fullpower是一家领先的生物传感AI解决方案提供商。![图片](https://ai-techpark.com/wp-content/uploads/Somnigroup.jpg){ width=60% }
+
+---
+自2019年以来，Fullpower的Sleeptracker-AI（“Sleeptracker”）技术已经为TEMPUR-Ergo®智能底座系列提供了超过2.5亿夜的睡眠数据。作为扩大合作的一部分，Tempur Sealy（Somnigroup的全资子公司）向Fullpower进行2500万美元的股权投资，以收购约15.6%的 ownership stakes。
+
+此外，Tempur Sealy和Fullpower还签署了一项为期多年的延长协议，通过2036年，继续拥有独家权利在其产品中嵌入Sleeptracker技术。该协议确保了继续访问行业内最先进的AI睡眠平台之一，确保全球消费者能够持续获得睡眠洞察和指导。进一步来说，Sleeptracker所收集的睡眠数据使Tempur Sealy能够更深入地理解消费者行为，并提供更为针对性的产品解决方案。
+
+Somnigroup董事长兼首席执行官Scott Thompson评论道：“这一战略投资反映了我们在推动创新和增强消费者体验未来方面的深切承诺。通过延长与Fullpower的关系，我们不仅进一步提升了为消费者提供更智能、更个性化解决方案的能力，同时也巩固了我们在卧具创新领域的领先地位。”
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
