a3a82fdcc9754d41c3b9c3db4118f3b6cfed284f
web3nb <jesonchen@imoocc.com>
2025-08-15 19:10:05 +0000

    Commit on Fri Aug 15 19:10:05 UTC 2025

---
+++ posts/2025-08-15-auto3-q0PvoY.rb
@@ -0,0 +1,25 @@
+title: 'EON 2025亮点：应用AI创新与真实影响'
+date: 2025-08-16
+author: ByteAILab
+
+---
+
+EON，年度顶级应用AI盛会，将于10月29日至30日在Lido House重返，汇聚来自财富500强公司的顶尖创新者和早期深科技初创公司。![图片](https://ai-techpark.com/wp-content/uploads/EON-2025.jpg){ width=60% }
+
+---
+在为期两天的活动中，与会者将探索变革制造业、医疗保健和金融等行业的下一代AI技术，并通过机会建立新的合作伙伴关系和客户关系。
+
+EON最近公布了其卓越的顾问委员会和主题演讲嘉宾：Cobot的Brad Porter、索尼集团公司和冲绳科学技术大学院的Hiroaki Kitano，以及Agility Robotics的Jonathan Hurst。此次活动还将围绕“应用AI：实现承诺”的主题召开无主题的讨论会。
+
+EON还兴奋地宣布将进行深度探讨的物理AI小组讨论，展示GrayMatter Robotics的联合创始人及首席执行官Ariyan Kabir，以及该公司的客户Oshkosh Corporation的Pierce Manufacturing（领先的消防车制造商）、Riddell（足球头盔保护及运动保护装备的领导者）和IAC（国际航空涂料公司，领先的飞机喷涂和航空服务提供商）。
+
+“我们很高兴能回到EON，与AI创新者社区重新联系，”Kabir说。“能够将GrayMatter Robotics及我们杰出的合作伙伴，如Pierce、Riddell和IAC，展示出来，我们感到无比荣幸。我们期待分享他们在AI、机器人技术和先进制造方面的旅程，以及他们所克服的挑战。”
+
+EON由Calibrate Ventures的联合创始人兼管理合伙人Jason Schoettler和Kevin Dunlap创办，是一家与深科技创始人合作的早期风险投资公司。
+
+“这是EON的第四届年度活动，预计将是最具影响力的一届，”Dunlap表示。“我们的顾问委员会和演讲者将探索AI的未来，而像GrayMatter这样的讨论小组将使讨论扎根于实际应用。今年的EON将为与会者清晰描绘出技术将何去何从，以及它如何创造价值。”
+
+过去三年EON活动均已售罄，预计2025年也将迅速满员。点击此处申请参加或咨询赞助机会。
+
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
