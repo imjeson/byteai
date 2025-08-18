2365f96fdd9be983fc9bbd9c04a50a2336802b3d
web3nb <jesonchen@imoocc.com>
2025-08-18 19:10:03 +0000

    Commit on Mon Aug 18 19:10:03 UTC 2025

---
+++ posts/2025-08-18-auto3-gqsE0M.rb
@@ -0,0 +1,29 @@
+title: 'NewRocket任命Jason Rosenfeld为首席增长与联盟官'
+date: 2025-08-19
+author: ByteAILab
+
+---
+
+NewRocket，一家领先的数字工作流和人工智能顾问及Elite ServiceNow合作伙伴，今天宣布已任命Jason Rosenfeld为公司的首席增长与联盟官（CGAO），这一新设的执行角色旨在通过深化NewRocket与ServiceNow的合作关系及为关键客户提供卓越成果来推动战略增长。![图片](https://ai-techpark.com/wp-content/uploads/NewRocket.jpg){ width=60% }
+
+---
+Rosenfeld在加入NewRocket之前曾担任Cask NX的首席执行官，该公司是一家专注于端到端业务转型的纯ServiceNow Elite合作伙伴，他将带着在ServiceNow各方面的咨询和专业服务中的领导思想和经验，来执行NewRocket的远见倡议。NewRocket得到中型市场私募投资公司Gryphon Investors（“Gryphon”）的支持。
+
+Rosenfeld带着超过二十年的技术服务、企业合作关系和数字创新的经验加入NewRocket。作为CGAO，他将负责领导与ServiceNow的合作，建立新联盟和客户顾问委员会，扩展关键行业和地区的战略客户关系。他还将主导公司市场营销，并与顶级PE公司合作开辟新的收入渠道。
+
+“Jason在ServiceNow生态系统中是一个经过验证的增长领袖，我非常高兴欢迎他加入NewRocket，”首席执行官Harsha Kumar表示。“他在ServiceNow的差异化经验将对我们在全球范围内扩大与战略客户的业务发挥重要作用。凭借他在技术解决方案、客户成功、销售和高管管理等多个领域的全面背景，Jason将对我们短期和长期的增长战略产生实际影响。”
+
+Rosenfeld是全球少数能够担任ServiceNow合作伙伴几乎所有重要角色的领导者之一，从顾问到客户执行、第一线领导，再到负责战略、增长、市场营销、销售、联盟和创新团队，他的最后一个角色是500人规模的有机生长Elite合作伙伴的首席执行官。
+
+在进入ServiceNow生态系统之前，Rosenfeld在惠普领导高效能团队，专注于该公司的最大服务账户的服务管理和数据工程。他还在EDS为航空和运输业领导现场服务和工程团队，并在普华永道担任管理顾问，实施领先的客户关系管理解决方案。
+
+“Jason是一位优秀的领导者，我们有幸与他建立了近五年的关系。我们一直在关注他的成长和成功，他的任命反映了我们继续致力于在NewRocket投资世界级领导力的承诺，”Gryphon Investors的技术解决方案与服务部门合伙人兼负责人Gabe Stephenson表示。“他在领导公司、拓展战略账户和推动这一领域的创新方面拥有十多年丰富的经验，将对帮助NewRocket进一步巩固其作为全球领先的ServiceNow专注服务公司的地位起到重要作用。”
+
+Rosenfeld评论道：“我非常激动能够加入NewRocket团队！公司和Gryphon正在进行的投资，确保NewRocket不仅成为全球顶尖ServiceNow合作伙伴，还成为AI愿景和执行方面的顶级咨询服务领导者，这让我感到兴奋。NewRocket在过去一年组建的领导团队展现了领先于所有技术驱动服务公司的承诺，无论是在ServiceNow生态系统内外，我一想到即将到来的机遇就感到鸡皮疙瘩。”
+
+Rosenfeld拥有利哈伊大学的工业工程学士学位，常驻NewRocket位于加利福尼亚州圣地亚哥的总部。
+
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
