ad1bf1102dc94d42ec4dae01d4c880b0d9c5c4a7
web3nb <jesonchen@imoocc.com>
2025-08-19 19:10:04 +0000

    Commit on Tue Aug 19 19:10:03 UTC 2025

---
+++ posts/2025-08-19-auto3-l3OGhk.rb
@@ -0,0 +1,19 @@
+title: 'Gather AI与Burwell合作推出人工智能驱动的仓库解决方案'
+date: 2025-08-20
+author: ByteAILab
+
+---
+
+今天，领先的人工智能驱动的内部物流公司Gather AI宣布与Burwell Material Handling达成首个经销商网络合作伙伴关系，后者将在美国20个地点向客户分发Gather AI的无人机和物料搬运设备（MHE）视觉摄像头解决方案。![图片](https://ai-techpark.com/wp-content/uploads/Gather-AI.jpg){ width=60% }
+
+---
+
+“我们始终在寻找能够帮助客户更好地管理仓库和优化订单履行的解决方案，”Burwell Material Handling首席技术官Jeff Buyck说道。“我们选择与Gather AI合作，因为我们能够帮助客户改善库存准确性，跟踪损坏或过期库存，并最终更好地为客户服务。”
+Gather AI的人工智能启用机器视觉技术源于卡内基梅隆大学，其通过与MHE配对，能够扫描的不仅是条形码：还包括批号、文本、过期日期、箱子数量、损坏情况、占用率等信息。
+Gather AI的软件可以与任何设置集成，无需基础设施更改。它同样适应多种环境——在冷藏库（-20°F）、黑暗仓库和非常窄的过道中（无架子或改装）无需GPS或Wi-Fi。客户的库存准确性提高了70%，盘点时间减少了75%，托盘紧急情况减少了90%，运营生产力提升了5倍。
+“Gather AI致力于帮助仓库优化其运营，”Gather AI首席执行官兼联合创始人Sankalp Arora博士表示。“这项合作将帮助Burwell Material Handling扩大他们的产品供应，以满足客户的需求，并使更多公司能够访问我们的解决方案。”
+其他感兴趣的MHE经销商可访问www.gather.ai/contact，了解与Gather AI合作的机会。欲询问如何将Gather AI的解决方案添加到您的物料搬运操作中，请访问burwellmh.com。
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
