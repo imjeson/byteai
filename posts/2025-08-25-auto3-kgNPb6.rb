23152c5ab9f800d77c7a476a79e784f3b1935da3
web3nb <jesonchen@imoocc.com>
2025-08-25 19:10:05 +0000

    Commit on Mon Aug 25 19:10:04 UTC 2025

---
+++ posts/2025-08-25-auto3-kgNPb6.rb
@@ -0,0 +1,28 @@
+title: 'INTEGRITY推出TrafficAuth-Mobile™提升交通安全'
+date: 2025-08-26
+author: ByteAILab
+
+---
+
+行业领先的新应用确保交通网络间的互操作性、标准合规性和安全无线通信  
+INTEGRITY Security Services (ISS)今天宣布推出TrafficAuth-Mobile，这是首个车辆至网络至一切（V2N2X）应用程序，能够在蜂窝网络和快速增长的直接V2X交通网络之间实现消息的安全互操作性。![图片](https://ai-techpark.com/wp-content/uploads/INTEGRITY.jpg){ width=60% }
+
+---
+  
+“美国交通网络的安全和可靠通信的未来，取决于创造能够互操作、符合标准和安全的产品与解决方案，以提供车辆、移动设备、交通信号灯等路边设备和整个智能交通生态系统之间的连接，”ISS连接车辆解决方案副总裁兼总经理Brian Romansky表示。“TrafficAuth-Mobile为V2N2X通信建立了一个全新的基础，将提升道路系统和运营的效率，同时帮助美国司机和运输工人保持安全、连接和保障。”
+
+作为ISS TrafficAuth系列安全连接解决方案的一部分，TrafficAuth-Mobile使Android和iOS移动设备能够与其他交通应用程序（包括连接汽车、交通基础设施和其他移动用户）发送和接收安全消息。为道路工作者提供的高级功能，为提升工人安全提供了一种低成本的解决方案。对于服务提供商和其他运输供应商，TrafficAuth-SCMSclient库也允许他们在自己的应用程序中嵌入同样的V2N2X通信功能。
+
+基于ISS超过30年的经验和安全保护超过十亿嵌入式设备的专业知识，TrafficAuth-Mobile应用程序及其相关工具包具有以下特点：
+
+- 一个安全、集成和完全互操作的一体化V2N2X解决方案。
+- 完整的端到端安全凭证管理和授权。
+- 网络V2N和直接V2X消息之间的无缝互操作性。
+- 能够与路边设备互动，简化运输服务，并直接向工人和司机发送施工区域安全或交通消息。
+- 符合IEEE 1609.2和SAE J2735标准、SCMS管理政策以及交通部对安全凭证管理系统（SCMS）资金的要求。
+
+ISS将在2025年8月24日至28日在乔治亚州亚特兰大的ITS世界大会上提供TrafficAuth-Mobile应用程序的现场演示。欲了解更多信息，请访问我们的展位（展位553）或联系ISS，邮箱为info@ghsiss.com。
+
+---
+---
+感谢阅读！如果您对AI的更多资讯感兴趣，可以查看更多AI文章：[GPTNB](https://gptnb.com)。
